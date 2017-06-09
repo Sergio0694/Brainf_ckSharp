bd5efa4fa53359940cb249ab2200c85f01a38841
Sergio Pedri <Sergio0694@live.com>
2017-06-09 11:49:06 +0200

    UI effects added to the Shell

--- Brainf_ck-sharp.UWP/UserControls/Shell.xaml.cs
+++ Brainf_ck-sharp.UWP/UserControls/Shell.xaml.cs
@@ -1,6 +1,11 @@
-﻿using Windows.UI.Xaml.Controls;
-
-// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+﻿using System;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Brainf_ck_sharp_UWP.Helpers.WindowsAPIs;
+using UICompositionAnimations.Behaviours;
+using UICompositionAnimations.Behaviours.Effects.Base;
 
 namespace Brainf_ck_sharp_UWP.UserControls
 {
@@ -8,7 +13,47 @@ namespace Brainf_ck_sharp_UWP.UserControls
     {
         public Shell()
         {
+            // Hide the title placeholder if needed
+            if (UniversalAPIsHelper.IsMobileDevice)
+            {
+                PCPlaceholderGrid.Visibility = Visibility.Collapsed;
+                KeyboardCanvas.Visibility = Visibility.Collapsed;
+                KeyboardBorder.Visibility = Visibility.Collapsed;
+            }
+
+            // Update the effects and initialize the UI
+            this.Loaded += Shell_Loaded;
+            this.SizeChanged += (s, e) =>
+            {
+                _HeaderEffect?.AdjustSize();
+                _KeyboardEffect?.AdjustSize();
+            };
             this.InitializeComponent();
+        }
+
+        // Acrylic brush for the header
+        private AttachedStaticCompositionEffect<Border> _HeaderEffect;
+
+        // Acrylic brush for the virtual keyboard
+        private AttachedStaticCompositionEffect<Border> _KeyboardEffect;
+
+        // Initialize the effects
+        private async void Shell_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (UniversalAPIsHelper.IsMobileDevice)
+            {
+                _HeaderEffect = await HeaderBorder.GetAttachedInAppSemiAcrylicEffectAsync(HeaderBorder, 16, 800,
+                    Color.FromArgb(byte.MaxValue, 30, 30, 30), 0.8f,
+                    HeaderCanvas, new Uri("ms-appx:///Assets/Misc/noise.png"));
+                OperatorsKeyboard.Background = new SolidColorBrush(Color.FromArgb(byte.MaxValue, 10, 10, 10));
+            }
+            else
+            {
+                _HeaderEffect = await HeaderBorder.GetAttachedSemiAcrylicEffectAsync(Color.FromArgb(byte.MaxValue, 30, 30, 30), 0.8f,
+                    HeaderCanvas, new Uri("ms-appx:///Assets/Misc/noise.png"));
+                _KeyboardEffect = await KeyboardBorder.GetAttachedSemiAcrylicEffectAsync(Color.FromArgb(byte.MaxValue, 16, 16, 16), 0.95f,
+                    KeyboardCanvas, new Uri("ms-appx:///Assets/Misc/noise.png"));
+            }
         }
     }
 }
