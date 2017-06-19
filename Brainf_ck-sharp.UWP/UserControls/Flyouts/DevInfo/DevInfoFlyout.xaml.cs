40a0e9d1ef0dbe7f98d21075f73937158f6c155d
Sergio Pedri <Sergio0694@live.com>
2017-06-19 14:31:05 +0200

    Added donation IAP UI and implementation

--- Brainf_ck-sharp.UWP/UserControls/Flyouts/DevInfo/DevInfoFlyout.xaml.cs
+++ Brainf_ck-sharp.UWP/UserControls/Flyouts/DevInfo/DevInfoFlyout.xaml.cs
@@ -1,9 +1,15 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Windows.Services.Store;
 using Windows.System;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Brainf_ck_sharp_UWP.Helpers;
 using Brainf_ck_sharp_UWP.Helpers.Extensions;
+using Brainf_ck_sharp_UWP.Messages;
+using Brainf_ck_sharp_UWP.PopupService;
+using Brainf_ck_sharp_UWP.PopupService.Misc;
+using GalaSoft.MvvmLight.Messaging;
 
 namespace Brainf_ck_sharp_UWP.UserControls.Flyouts.DevInfo
 {
@@ -33,6 +39,79 @@ namespace Brainf_ck_sharp_UWP.UserControls.Flyouts.DevInfo
         private void TwitterButton_Click(object sender, RoutedEventArgs e)
         {
             Launcher.LaunchUriAsync(new Uri("https://twitter.com/sergiopedri")).AsTask().Forget();
+        }
+
+        // Show the donation options
+        private async void DonateButton_Click(object sender, RoutedEventArgs e)
+        {
+            DevSupportPickerFlyout flyout = new DevSupportPickerFlyout();
+            FlyoutClosedResult<int> result = await FlyoutManager.Instance.ShowAsync<DevSupportPickerFlyout, int>(
+                LocalizationManager.GetResource("Donate"), flyout, new Thickness(0), FlyoutDisplayMode.ActualHeight, true);
+            if (result) ProcessDonationAsync(result.Value).Forget();
+        }
+
+        // In-app products
+        private const String CoffeeInAppStoreId = ""; // TODO
+        private const String PresentInAppStoreId = "";
+        private const String DonateInAppStoreId = "";
+        private const String VIPSupportInAppStoreId = "";
+
+        /// <summary>
+        /// Processes a donation in the Store
+        /// </summary>
+        /// <param name="option">The index of the target IAP to buy</param>
+        public static async Task ProcessDonationAsync(int option)
+        {
+            // Get the id of the product to purchase
+            String id;
+            switch (option)
+            {
+                case 1: id = CoffeeInAppStoreId; break;
+                case 2: id = PresentInAppStoreId; break;
+                case 3: id = DonateInAppStoreId; break;
+                case 4: id = VIPSupportInAppStoreId; break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+
+            // Try to purchase the item
+            Messenger.Default.Send(new AppLoadingStatusChangedMessage(true));
+            StorePurchaseResult result;
+            StoreContext store = StoreContext.GetDefault();
+            try
+            {
+                result = await store.RequestPurchaseAsync(id);
+            }
+            catch
+            {
+                NotificationsManager.ShowDefaultErrorNotification(
+                    LocalizationManager.GetResource("StoreConnectionError"), LocalizationManager.GetResource("StoreConnectionErrorBody"));
+                return;
+            }
+            finally
+            {
+                Messenger.Default.Send(new AppLoadingStatusChangedMessage(false));
+            }
+
+            // Display the result
+            switch (result.Status)
+            {
+                case StorePurchaseStatus.Succeeded:
+                    NotificationsManager.ShowNotification(0xEC24.ToSegoeMDL2Icon(), LocalizationManager.GetResource("DonationCompleted"), "s", NotificationType.Default);
+                    store.ReportConsumableFulfillmentAsync(id, 1, Guid.NewGuid()).AsTask().Forget();
+                    break;
+                case StorePurchaseStatus.NotPurchased:
+                    NotificationsManager.ShowDefaultErrorNotification(LocalizationManager.GetResource("PurchaseCanceled"), LocalizationManager.GetResource("PurchaseCanceledBody"));
+                    break;
+                case StorePurchaseStatus.AlreadyPurchased:
+                    store.ReportConsumableFulfillmentAsync(id, 1, Guid.NewGuid()).AsTask().Forget();
+                    NotificationsManager.ShowDefaultErrorNotification($"{LocalizationManager.GetResource("SomethingBadHappened")} :'(", LocalizationManager.GetResource("DonationErrorBody"));
+                    break;
+                default:
+                    // Error
+                    NotificationsManager.ShowDefaultErrorNotification($"{LocalizationManager.GetResource("SomethingBadHappened")} :'(", LocalizationManager.GetResource("DonationErrorBody"));
+                    break;
+            }
         }
     }
 }
---
+++ Brainf_ck-sharp.UWP/UserControls/Flyouts/DevInfo/DevSupportOptionEntryControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+
+namespace Brainf_ck_sharp_UWP.UserControls.Flyouts.DevInfo
+{
+    public sealed partial class DevSupportOptionEntryControl : UserControl
+    {
+        public DevSupportOptionEntryControl()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets the image to display inside the control
+        /// </summary>
+        public ImageSource ImageData
+        {
+            get => EntryImage.Source;
+            set => EntryImage.Source = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the title of the control
+        /// </summary>
+        public String PickerTitle
+        {
+            get => this.TitleBlock.Text;
+            set => this.TitleBlock.Text = value;
+        }
+
+        /// <summary>
+        /// Gets or sets the description of the control
+        /// </summary>
+        public String PickerDescription
+        {
+            get => this.DescriptionBlock.Text;
+            set => this.DescriptionBlock.Text = value;
+        }
+    }
+}
