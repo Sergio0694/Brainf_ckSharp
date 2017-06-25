7f1f6b11802c80f37be429a5949eebd2e8e9a3a3
Sergio Pedri <Sergio0694@live.com>
2017-06-25 13:09:41 +0200

    Fixed memory leak with the InAppAcrylicGrid control

--- Brainf_ck-sharp.UWP/UserControls/InheritedControls/InAppAcrylicGrid.cs
+++ Brainf_ck-sharp.UWP/UserControls/InheritedControls/InAppAcrylicGrid.cs
@@ -19,17 +19,34 @@ namespace Brainf_ck_sharp_UWP.UserControls.InheritedControls
             Loaded += InAppAcrylicGrid_Loaded;
         }
 
+        /// <summary>
+        /// Gets or sets whether or not native resources should be disposed when the control is unloaded
+        /// </summary>
+        public bool DisposeOnUnload { get; set; }
+
         // Initializes the effect when the control is rendered for the first time
         private async void InAppAcrylicGrid_Loaded(object sender, RoutedEventArgs e)
         {
+            // UI setup
+            Loaded -= InAppAcrylicGrid_Loaded;
             if (Children.Contains(_Win2DCanvas)) return;
             Children.Add(_Win2DCanvas);
             await this.AttachCompositionInAppCustomAcrylicEffectAsync(this, 8, 800,
                 Color.FromArgb(byte.MaxValue, 34, 34, 34), 0.6f, null,
-                _Win2DCanvas, new Uri("ms-appx:///Assets/Misc/noise.png"));
+                _Win2DCanvas, new Uri("ms-appx:///Assets/Misc/noise.png"), disposeOnUnload: DisposeOnUnload);
+
+            // Dispose setup
+            if (DisposeOnUnload)
+            {
+                Unloaded += (s, _) =>
+                {
+                    _Win2DCanvas.RemoveFromVisualTree();
+                    _Win2DCanvas = null;
+                };
+            }
         }
 
         // The canvas used to render part of the effect
-        private readonly CanvasControl _Win2DCanvas;
+        private CanvasControl _Win2DCanvas;
     }
 }
