736c68d439ac66b9527f102e8d7282fba318c9aa
Sergio Pedri <Sergio0694@live.com>
2017-06-30 11:26:32 +0200

    Minor UI adjustments to the changelog view, Telerik UI updated

--- Brainf_ck-sharp.UWP/ViewModels/FlyoutsViewModels/ChangelogViewFlyoutViewModel.cs
+++ Brainf_ck-sharp.UWP/ViewModels/FlyoutsViewModels/ChangelogViewFlyoutViewModel.cs
@@ -30,7 +30,7 @@ namespace Brainf_ck_sharp_UWP.ViewModels.FlyoutsViewModels
             // Create the output collection
             return new List<JumpListGroup<ChangelogReleaseInfo, IReadOnlyList<String>>>
             {
-                CreateChangelogEntry("1.1.1.0", 2017, 7, 4, new List<String>
+                CreateChangelogEntry("1.1.1.0", 2017, 7, 5, new List<String>
                 {
                     "Added a button to delete the previous character in the IDE",
                     "It is no longer possible to try to run code with a syntax error from the IDE",
