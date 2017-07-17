411da8be19f804442c40a0c99c2b04b7346185ac
Sergio Pedri <Sergio0694@live.com>
2017-07-17 23:10:50 +0200

    Added theme preview template

---
+++ Brainf_ck-sharp.UWP/UserControls/DataTemplates/IDEThemes/IDEThemePreviewTemplate.xaml.cs
@@ -0,0 +1,16 @@
+﻿using Windows.UI.Xaml.Controls;
+using Brainf_ck_sharp_UWP.DataModels.Misc;
+using Brainf_ck_sharp_UWP.Helpers.Extensions;
+
+namespace Brainf_ck_sharp_UWP.UserControls.DataTemplates.IDEThemes
+{
+    public sealed partial class IDEThemePreviewTemplate : UserControl
+    {
+        public IDEThemePreviewTemplate()
+        {
+            this.InitializeComponent();
+        }
+
+        public IDEThemeInfo ViewModel => DataContext.To<IDEThemeInfo>();
+    }
+}
