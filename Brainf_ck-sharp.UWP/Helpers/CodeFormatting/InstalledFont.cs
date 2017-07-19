1a67dbda9f47e3d3cc3aa4adbec34226e069f3ac
Sergio Pedri <Sergio0694@live.com>
2017-07-19 16:35:40 +0200

    Added InstalledFont class, SharpDX package

---
+++ Brainf_ck-sharp.UWP/Helpers/CodeFormatting/InstalledFont.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using JetBrains.Annotations;
+using SharpDX.DirectWrite;
+
+namespace Brainf_ck_sharp_UWP.Helpers.CodeFormatting
+{
+    /// <summary>
+    /// A class that represents an installed font on the current device
+    /// </summary>
+    public class InstalledFont
+    {
+        /// <summary>
+        /// Gets the name of the font
+        /// </summary>
+        [NotNull]
+        public String Name { get; }
+
+        private Windows.UI.Xaml.Media.FontFamily _Family;
+
+        /// <summary>
+        /// Gets the family of font to use in a text control
+        /// </summary>
+        public Windows.UI.Xaml.Media.FontFamily Family => _Family ?? (_Family = new Windows.UI.Xaml.Media.FontFamily(Name));
+
+        private static IReadOnlyList<InstalledFont> _Fonts;
+
+        /// <summary>
+        /// Gets the list of available fonts on the current device
+        /// </summary>
+        [NotNull]
+        public static IReadOnlyList<InstalledFont> Fonts => _Fonts ?? (_Fonts = GetFonts());
+
+        // Private constructor
+        private InstalledFont([NotNull] String name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// Returns a list of the available font on the current device
+        /// </summary>
+        [NotNull]
+        private static IReadOnlyList<InstalledFont> GetFonts()
+        {
+            // Prepare the loop
+            List<InstalledFont> fontList = new List<InstalledFont>();
+            Factory factory = new Factory();
+            FontCollection fontCollection = factory.GetSystemFontCollection(false);
+            int familyCount = fontCollection.FontFamilyCount;
+
+            // Iterate over the available fonts
+            for (int i = 0; i < familyCount; i++)
+            {
+                // Get the current font family
+                FontFamily fontFamily = fontCollection.GetFontFamily(i);
+                LocalizedStrings familyNames = fontFamily.FamilyNames;
+
+                // Get the local name of the font
+                if (!familyNames.FindLocaleName(CultureInfo.CurrentCulture.Name, out int index))
+                {
+                    familyNames.FindLocaleName("en-us", out index);
+                }
+
+                // Gets the actual font family name to return
+                String name = familyNames.GetString(index);
+                if (AllowedFonts.Contains(name)) fontList.Add(new InstalledFont(name));
+            }
+            return fontList.OrderBy(font => font.Name).ToArray();
+        }
+
+        /// <summary>
+        /// Gets the list of allowed font families
+        /// </summary>
+        [NotNull]
+        private static readonly ISet<String> AllowedFonts = new HashSet<String>
+        {
+            "Arial",
+            "Calibri",
+            "Cambria",
+            "Candara",
+            "Consolas",
+            "Segoe UI"
+        };
+    }
+}
