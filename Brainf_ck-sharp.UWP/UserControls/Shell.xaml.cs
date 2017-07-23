dce1bdc342b63447cd2bfa91b82a854df9f715a7
Sergio Pedri <Sergio0694@live.com>
2017-07-23 16:30:35 +0200

    Minor settings UI improvements

--- Brainf_ck-sharp.UWP/UserControls/Shell.xaml.cs
+++ Brainf_ck-sharp.UWP/UserControls/Shell.xaml.cs
@@ -276,8 +276,8 @@ namespace Brainf_ck_sharp_UWP.UserControls
             bool whitespaces = AppSettingsManager.Instance.GetValue<bool>(nameof(AppSettingsKeys.RenderWhitespaces));
             String font = AppSettingsManager.Instance.GetValue<String>(nameof(AppSettingsKeys.SelectedFontName));
             SettingsPanelFlyout settings = new SettingsPanelFlyout();
-            settings.ViewModel.LoadGroupsAsync().Forget();
-            await FlyoutManager.Instance.ShowAsync(LocalizationManager.GetResource("Settings"), settings);
+            Task.Delay(100).ContinueWith(t => settings.ViewModel.LoadGroupsAsync().Forget(), TaskScheduler.FromCurrentSynchronizationContext()).Forget();
+            await FlyoutManager.Instance.ShowAsync(LocalizationManager.GetResource("Settings"), settings, new Thickness());
             bool
                 themeChanged = AppSettingsManager.Instance.GetValue<int>(nameof(AppSettingsKeys.SelectedIDETheme)) != theme,
                 tabsChanged = AppSettingsManager.Instance.GetValue<int>(nameof(AppSettingsKeys.TabLength)) != tabs,
--- Brainf_ck-sharp.UWP/ViewModels/FlyoutsViewModels/Settings/SettingsJumpListViewModel.cs
+++ Brainf_ck-sharp.UWP/ViewModels/FlyoutsViewModels/Settings/SettingsJumpListViewModel.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using Brainf_ck_sharp_UWP.DataModels;
 using Brainf_ck_sharp_UWP.DataModels.Settings;
@@ -10,17 +7,17 @@ using Brainf_ck_sharp_UWP.ViewModels.Abstract;
 
 namespace Brainf_ck_sharp_UWP.ViewModels.FlyoutsViewModels.Settings
 {
-    public class SettingsJumpListViewModel : JumpListViewModelBase<Tuple<String, int>, CategorizedSettingsViewModel>
+    public class SettingsJumpListViewModel : JumpListViewModelBase<Tuple<SettingsSectionType, int>, CategorizedSettingsViewModel>
     {
         private SettingsViewModel Settings { get; } = new SettingsViewModel();
 
-        protected override Task<IList<JumpListGroup<Tuple<String, int>, CategorizedSettingsViewModel>>> OnLoadGroupsAsync()
+        protected override Task<IList<JumpListGroup<Tuple<SettingsSectionType, int>, CategorizedSettingsViewModel>>> OnLoadGroupsAsync()
         {
-            IList<JumpListGroup<Tuple<String, int>, CategorizedSettingsViewModel>> list = new List<JumpListGroup<Tuple<String, int>, CategorizedSettingsViewModel>>
+            IList<JumpListGroup<Tuple<SettingsSectionType, int>, CategorizedSettingsViewModel>> list = new List<JumpListGroup<Tuple<SettingsSectionType, int>, CategorizedSettingsViewModel>>
             {
-                new JumpListGroup<Tuple<String, int>, CategorizedSettingsViewModel>(Tuple.Create("IDE", 6), 
+                new JumpListGroup<Tuple<SettingsSectionType, int>, CategorizedSettingsViewModel>(Tuple.Create(SettingsSectionType.IDE, 7), 
                     new[] { new CategorizedSettingsViewModel(SettingsSectionType.IDE, Settings) }),
-                new JumpListGroup<Tuple<String, int>, CategorizedSettingsViewModel>(Tuple.Create("UI", 2), 
+                new JumpListGroup<Tuple<SettingsSectionType, int>, CategorizedSettingsViewModel>(Tuple.Create(SettingsSectionType.UI, 2), 
                     new[] { new CategorizedSettingsViewModel(SettingsSectionType.UI, Settings) })
             };
             return Task.FromResult(list);
