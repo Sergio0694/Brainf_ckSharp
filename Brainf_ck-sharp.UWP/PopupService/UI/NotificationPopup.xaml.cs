18dd2922a5f8f667f2821444b68bfda1038ca896
Sergio Pedri <Sergio0694@live.com>
2017-09-02 02:07:45 +0200

    Minor UI tweaks to the notifications

--- Brainf_ck-sharp.UWP/PopupService/UI/NotificationPopup.xaml.cs
+++ Brainf_ck-sharp.UWP/PopupService/UI/NotificationPopup.xaml.cs
@@ -1,15 +1,12 @@
 ﻿using System;
-using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media;
 using Brainf_ck_sharp_UWP.Helpers;
 using Brainf_ck_sharp_UWP.Messages.Flyouts;
 using Brainf_ck_sharp_UWP.PopupService.Misc;
-using Brainf_ck_sharp_UWP.Resources;
 using GalaSoft.MvvmLight.Messaging;
 using JetBrains.Annotations;
 using UICompositionAnimations;
-using UICompositionAnimations.Behaviours;
+using UICompositionAnimations.Brushes;
 using UICompositionAnimations.Enums;
 using UICompositionAnimations.Helpers.PointerEvents;
 
@@ -26,35 +23,17 @@ namespace Brainf_ck_sharp_UWP.PopupService.UI
         /// <param name="type">Indicates the kind of notification to show</param>
         public NotificationPopup([NotNull] String title, [NotNull] String icon, [NotNull] String content, NotificationType type)
         {
-            Loaded += NotificationPopup_Loaded;
             this.InitializeComponent();
+            Root.Background = XAMLResourcesHelper.GetResourceValue<CustomAcrylicBrush>(
+                type == NotificationType.Default ? "NotificationAcrylicBrush" : "NotificationErrorAcrylicBrush");
             TitleBlock.Text = title;
             SymbolBlock.Text = icon;
             ContentBlock.Text = content;
-            Type = type;
             CloseButton.ManageLightsPointerStates(value =>
             {
                 BackgroundBorder.StartXAMLTransformFadeAnimation(null, value ? 0.6 : 0, 200, null, EasingFunctionNames.Linear);
-                LightBorder.StartXAMLTransformFadeAnimation(null, value ? 0 : 0.8, 200, null, EasingFunctionNames.Linear);
+                LightBorder.StartXAMLTransformFadeAnimation(null, value ? 0 : 0.2, 200, null, EasingFunctionNames.Linear);
             });
-            Unloaded += (s, e) =>
-            {
-                Win2DCanvas.RemoveFromVisualTree();
-                Win2DCanvas = null;
-            };
-        }
-
-        // Gets the current type of notification
-        private readonly NotificationType Type;
-
-        // Initializes the acrylic effect
-        private async void NotificationPopup_Loaded(object sender, RoutedEventArgs e)
-        {
-            await AcrylicBorder.AttachCompositionInAppCustomAcrylicEffectAsync(AcrylicBorder, 8, 800,
-                Type == NotificationType.Default 
-                ? BrushResourcesManager.Instance.AccentBrush.Color
-                : XAMLResourcesHelper.GetResourceValue<SolidColorBrush>("OrangeWarningBrush").Color, 0.5f, null,
-                Win2DCanvas, new Uri("ms-appx:///Assets/Misc/noise.png"), disposeOnUnload: true);
         }
 
         // Signals a request to close the notification
