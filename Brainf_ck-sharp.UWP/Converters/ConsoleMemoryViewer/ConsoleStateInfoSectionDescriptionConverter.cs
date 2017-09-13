1e698f674a28a048197069f5bdb3584e73ff5c8a
Sergio Pedri <Sergio0694@live.com>
2017-09-14 00:42:55 +0200

    Minor refactoring to the console memory viewer templates

---
+++ Brainf_ck-sharp.UWP/Converters/ConsoleMemoryViewer/ConsoleStateInfoSectionDescriptionConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+using Brainf_ck_sharp_UWP.DataModels.ConsoleMemoryViewer;
+using Brainf_ck_sharp_UWP.Helpers;
+
+namespace Brainf_ck_sharp_UWP.Converters.ConsoleMemoryViewer
+{
+    /// <summary>
+    /// A converter that displays a description for each section in the console state viewer
+    /// </summary>
+    public class ConsoleStateInfoSectionDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            switch (value)
+            {
+                case MemoryViewerMemoryCellsSectionData state:
+                    return $"{state.State.Count} {LocalizationManager.GetResource("MemoryCells")}";
+                case MemoryViewerFunctionsSectionData state:
+                    return $"{state.Functions.Count} {LocalizationManager.GetResource(state.Functions.Count > 1 ? "DefinedFunctions" : "DefinedFunction")}";
+            }
+            return String.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
