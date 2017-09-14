59394476fa56dd5053fc653f7a088d51cf7d4903
Sergio Pedri <Sergio0694@live.com>
2017-09-15 01:08:23 +0200

    Added two loop code guide samples

---
+++ Brainf_ck-sharp.UWP/UserControls/Flyouts/UserGuide/CodeSamplesGuideControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Brainf_ck_sharp_UWP.UserControls.Flyouts.UserGuide
+{
+    public sealed partial class CodeSamplesGuideControl : UserControl
+    {
+        public CodeSamplesGuideControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
