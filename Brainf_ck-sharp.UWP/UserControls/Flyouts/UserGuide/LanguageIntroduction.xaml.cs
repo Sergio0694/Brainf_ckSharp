6a7815e980316025f3ba116b49b5b3b846b78f3c
Sergio Pedri <Sergio0694@live.com>
2017-09-14 20:57:31 +0200

    Added base introduction section

---
+++ Brainf_ck-sharp.UWP/UserControls/Flyouts/UserGuide/LanguageIntroduction.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Brainf_ck_sharp_UWP.UserControls.Flyouts.UserGuide
+{
+    public sealed partial class LanguageIntroduction : UserControl
+    {
+        public LanguageIntroduction()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
