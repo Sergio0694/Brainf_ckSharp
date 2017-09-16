f1d8d2756f7274744964e33c7f16965013308f6b
Sergio Pedri <Sergio0694@live.com>
2017-09-16 16:48:00 +0200

    Added user guide header descriptions

---
+++ Brainf_ck-sharp.UWP/Converters/UserGuideSectionDescriptionConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Windows.UI.Xaml.Data;
+using Brainf_ck_sharp_UWP.Enums;
+using Brainf_ck_sharp_UWP.Helpers;
+using Brainf_ck_sharp_UWP.Helpers.Extensions;
+
+namespace Brainf_ck_sharp_UWP.Converters
+{
+    /// <summary>
+    /// A simple converter for the description of the user guide sections
+    /// </summary>
+    public class UserGuideSectionDescriptionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            switch (value.To<UserGuideSection>())
+            {
+                case UserGuideSection.Introduction: return LocalizationManager.GetResource("GuideIntroductionDescription");
+                case UserGuideSection.Samples: return LocalizationManager.GetResource("GuideCodeSamplesDescription");
+                case UserGuideSection.PBrain: return LocalizationManager.GetResource("GuidePBrainDescription");
+                default: return String.Empty;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Brainf_ck-sharp.UWP/UserControls/Flyouts/UserGuide/Sections/PBrainGuideControl.xaml.cs
+++ Brainf_ck-sharp.UWP/UserControls/Flyouts/UserGuide/Sections/PBrainGuideControl.xaml.cs
@@ -1,6 +1,6 @@
 ﻿using Windows.UI.Xaml.Controls;
 
-namespace Brainf_ck_sharp_UWP.UserControls.Flyouts.UserGuide
+namespace Brainf_ck_sharp_UWP.UserControls.Flyouts.UserGuide.Sections
 {
     public sealed partial class PBrainGuideControl : UserControl
     {
