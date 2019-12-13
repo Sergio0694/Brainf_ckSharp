cc7cd1c36cf65a54ac730ff516660decc73fedc2
Sergio Pedri <sergio0694@live.com>
2019-12-13 15:18:23 +0100

    Added UnsafeMemoryBuffer<T> and UnsafeMemory<T> types

---
+++ Brainf_ck-sharp.NET/Buffers/UnsafeMemoryBuffer{T}.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Buffers;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace Brainf_ck_sharp.NET.Buffers
+{
+    /// <summary>
+    /// A <see langword="class"/> that owns a memory buffer that can be used across stack frames
+    /// </summary>
+    /// <typeparam name="T">The type of items stored in the underlying buffer</typeparam>
+    internal unsafe class UnsafeMemoryBuffer<T> : IDisposable where T : unmanaged
+    {
+        /// <summary>
+        /// The size of the usable buffer within <see cref="Buffer"/>
+        /// </summary>
+        protected readonly int Size;
+
+        /// <summary>
+        /// The <typeparamref name="T"/> array that constitutes the memory buffer for the current instance
+        /// </summary>
+        private readonly T[] Buffer;
+
+        /// <summary>
+        /// A pointer to the first element in <see cref="Buffer"/>
+        /// </summary>
+        private readonly T* Ptr;
+
+        /// <summary>
+        /// The <see cref="GCHandle"/> instance used to pin <see cref="Buffer"/>
+        /// </summary>
+        /// <remarks>This field is not <see langword="readonly"/> to prevent the safe copy when calling <see cref="GCHandle.Free"/> from <see cref="Dispose"/></remarks>
+        private GCHandle _Handle;
+
+        /// <summary>
+        /// Creates a new <see cref="UnsafeMemoryBuffer{T}"/> instance with the specified parameters
+        /// </summary>
+        /// <param name="size">The size of the new memory buffer to use</param>
+        public UnsafeMemoryBuffer(int size)
+        {
+            Size = size;
+            Buffer = ArrayPool<T>.Shared.Rent(size);
+            _Handle = GCHandle.Alloc(Buffer, GCHandleType.Pinned);
+            Ptr = (T*)Unsafe.AsPointer(ref Buffer[0]);
+        }
+
+        /// <summary>
+        /// Gets an <see cref="UnsafeMemory{T}"/> instance mapping the values on the current buffer
+        /// </summary>
+        public UnsafeMemory<T> Memory
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => new UnsafeMemory<T>(Size, Ptr);
+        }
+
+        /// <summary>
+        /// Invokes <see cref="Dispose"/> to free the aallocated resources when this instance is destroyed
+        /// </summary>
+        ~UnsafeMemoryBuffer() => Dispose();
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            if (!_Handle.IsAllocated) return;
+
+            _Handle.Free();
+            ArrayPool<T>.Shared.Return(Buffer);
+
+            GC.SuppressFinalize(this);
+        }
+    }
+}
---
+++ Brainf_ck-sharp.NET/Buffers/UnsafeMemory{T}.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Brainf_ck_sharp.NET.Buffers
+{
+    /// <summary>
+    /// A <see langword="struct"/> that maps a range of <typeparamref name="T"/> values on an existing buffer
+    /// </summary>
+    /// <typeparam name="T">The type of items stored in the underlying buffer</typeparam>
+    internal readonly unsafe struct UnsafeMemory<T> where T : unmanaged
+    {
+        /// <summary>
+        /// The size of the usable buffer for the current instance
+        /// </summary>
+        public readonly int Size;
+
+        /// <summary>
+        /// A pointer to the first element in of the underlying buffer for the current instance
+        /// </summary>
+        private readonly T* Ptr;
+
+        /// <summary>
+        /// Creates a new <see cref="UnsafeMemory{T}"/> instance with the specified parameters
+        /// </summary>
+        /// <param name="size">The size of the new memory buffer to use</param>
+        /// <param name="ptr"></param>
+        public UnsafeMemory(int size, T* ptr)
+        {
+            if (size <= 0) throw new ArgumentOutOfRangeException(nameof(size), "The size must be a positive number");
+
+            Size = size;
+            Ptr = ptr;
+        }
+
+        /// <summary>
+        /// Gets the <typeparamref name="T"/> value at the specified index in the current buffer
+        /// </summary>
+        /// <param name="index">The target index to read the value from</param>
+        public T this[int index]
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Ptr[index];
+        }
+
+        /// <summary>
+        /// Gets a new <see cref="UnsafeMemory{T}"/> instance representing a view over the current buffer
+        /// </summary>
+        /// <param name="start">The inclusive starting index for the new <see cref="UnsafeMemory{T}"/> instance</param>
+        /// <param name="end">The exclusive ending index for the new <see cref="UnsafeMemory{T}"/> instance</param>
+        /// <returns>A new <see cref="UnsafeMemory{T}"/> instance mapping values in the [start, end) range on the current buffer</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public UnsafeMemory<T> Slice(int start, int end) => new UnsafeMemory<T>(end - start, Ptr + start);
+    }
+}
