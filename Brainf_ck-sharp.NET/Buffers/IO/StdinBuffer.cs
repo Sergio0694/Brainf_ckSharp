2f70d33774fb461a973b86e45efa72e39624b298
Sergio Pedri <sergio0694@live.com>
2019-12-14 11:16:37 +0100

    Added StdinBuffer and StdoutBuffer types

---
+++ Brainf_ck-sharp.NET/Buffers/IO/StdinBuffer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace Brainf_ck_sharp.NET.Buffers.IO
+{
+    /// <summary>
+    /// A <see langword="class"/> that represents a memory area to be used as stdin buffer
+    /// </summary>
+    internal sealed class StdinBuffer
+    {
+        /// <summary>
+        /// The underlying buffer to read characters from
+        /// </summary>
+        private readonly string Data;
+
+        /// <summary>
+        /// The current position in the underlying buffer to read from
+        /// </summary>
+        private int _Position;
+
+        /// <summary>
+        /// Creates a new <see cref="StdinBuffer"/> instance with the specified parameters
+        /// </summary>
+        /// <param name="data">The input data to use to read characters from</param>
+        public StdinBuffer(string data) => Data = data;
+
+        /// <summary>
+        /// Gets whether or not there are characters left in the current instance
+        /// </summary>
+        public bool CanRead
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => Data.Length > _Position;
+        }
+
+        /// <summary>
+        /// Reads a new character from the current instance
+        /// </summary>
+        /// <returns>The next <see cref="char"/> present in the underlying buffer</returns>
+        /// <exception cref="IndexOutOfRangeException">Thrown when the current buffer is exhausted</exception>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public char Read() => Data[_Position++];
+    }
+}
---
+++ Brainf_ck-sharp.NET/Buffers/IO/StdoutBuffer.cs
@@ -0,0 +1,41 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Brainf_ck_sharp.NET.Buffers.IO
+{
+    /// <summary>
+    /// A <see langword="class"/> that represents a memory area to be used as stdout buffer
+    /// </summary>
+    internal sealed class StdoutBuffer : UnsafeMemoryBuffer<char>
+    {
+        /// <summary>
+        /// The maximum allowed size for the output buffer
+        /// </summary>
+        public const int StdoutBufferSizeLimit = 1024;
+
+        /// <summary>
+        /// The current position in the underlying buffer to write to
+        /// </summary>
+        private int _Position;
+
+        public StdoutBuffer() : base(StdoutBufferSizeLimit) { }
+
+        /// <summary>
+        /// Gets whether or not new characters can be written to the current instance
+        /// </summary>
+        public bool CanWrite
+        {
+            [MethodImpl(MethodImplOptions.AggressiveInlining)]
+            get => _Position < StdoutBufferSizeLimit - 1;
+        }
+
+        /// <summary>
+        /// Writes a new character to the current instance
+        /// </summary>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public unsafe void Write(char c) => Ptr[_Position++] = c;
+
+        /// <inheritdoc/>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public override unsafe string ToString() => new string(Ptr, 0, _Position);
+    }
+}
