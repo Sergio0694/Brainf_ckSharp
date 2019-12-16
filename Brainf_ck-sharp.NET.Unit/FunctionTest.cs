bdc6d1d7519e93f080ac7cc1f49d66f9f28af77a
Sergio Pedri <sergio0694@live.com>
2019-12-16 12:19:23 +0100

    Added tests for PBrain functions

---
+++ Brainf_ck-sharp.NET.Unit/FunctionTest.cs
@@ -0,0 +1,49 @@
+﻿using Brainf_ck_sharp.NET.Enums;
+using Brainf_ck_sharp.NET.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Brainf_ck_sharp.NET.Unit
+{
+    [TestClass]
+    public class FunctionTest
+    {
+        [TestMethod]
+        public void SingleCall()
+        {
+            const string script = "+(,[>+<-]>.)>+:";
+
+            InterpreterResult result = Brainf_ckInterpreter.Run(script, "a");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.ExitCode, ExitCode.TextOutput);
+            Assert.AreEqual(result.MachineState.Current.Character, 'a');
+            Assert.AreEqual(result.Stdout, "a");
+        }
+
+        [TestMethod]
+        public void MultipleCalls()
+        {
+            const string script = "(+++):>:";
+
+            InterpreterResult result = Brainf_ckInterpreter.Run(script);
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.ExitCode, ExitCode.NoOutput);
+            Assert.AreEqual(result.MachineState[0].Value, 3);
+            Assert.AreEqual(result.MachineState[1].Value, 3);
+        }
+
+        [TestMethod]
+        public void Recursion()
+        {
+            const string script = ">,<(>[>+<-<:]):>[<<+>>-]<<.[-]";
+
+            InterpreterResult result = Brainf_ckInterpreter.Run(script, "%");
+
+            Assert.IsNotNull(result);
+            Assert.AreEqual(result.ExitCode, ExitCode.TextOutput);
+            Assert.AreEqual(result.MachineState.Current.Value, 0);
+            Assert.AreEqual(result.Stdout, "%");
+        }
+    }
+}
