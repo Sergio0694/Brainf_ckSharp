ccaa116bc2f038edbbe0681209250ec5a941e4c3
Sergio Pedri <sergio0694@live.com>
2019-12-22 17:08:58 +0100

    Added shell command bar and buttons

---
+++ src/Brainf_ckSharp.UWP/MarkupExtensions/BooleanExtension.cs
@@ -0,0 +1,19 @@
+﻿using Windows.UI.Xaml.Markup;
+
+namespace Brainf_ckSharp.UWP.MarkupExtensions
+{
+    /// <summary>
+    /// A markup extension that converts <see cref="bool"/> values in XAML to their boxed version
+    /// </summary>
+    [MarkupExtensionReturnType(ReturnType = typeof(object))]
+    public sealed class BooleanExtension : MarkupExtension
+    {
+        /// <summary>
+        /// Gets or sets the <see cref="bool"/> value for the current instance
+        /// </summary>
+        public bool Value { get; set; }
+
+        /// <inheritdoc/>
+        protected override object ProvideValue() => Value;
+    }
+}
