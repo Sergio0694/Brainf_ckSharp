54c442c43fab85d787df649848b249edcb309f1f
Sergio Pedri <sergio0694@live.com>
2019-12-22 22:16:53 +0100

    Added initial stdin header control

---
+++ src/Brainf_ckSharp.UWP/Controls/InputPanel/Header/StdinHeader.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Brainf_ckSharp.UWP.Controls.InputPanel.Header
+{
+    public sealed partial class StdinHeader : UserControl
+    {
+        public StdinHeader()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
