85cb5a58c6b493d892a26ad2cb97bf43b5c35e1d
Sergio Pedri <sergio0694@live.com>
2019-12-23 15:11:23 +0100

    Added ItemsCollectionViewModelBase<T> type

---
+++ src/Brainf_ckSharp.UWP/ViewModels/Abstract/ItemsCollectionViewModelBase{T}.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.ObjectModel;
+using GalaSoft.MvvmLight;
+
+#nullable enable
+
+namespace Brainf_ckSharp.UWP.ViewModels.Abstract
+{
+    /// <summary>
+    /// A view model for a view that exposes a collection of items of a given type
+    /// </summary>
+    /// <typeparam name="T">The type of items in the exposed collection</typeparam>
+    public abstract class ItemsCollectionViewModelBase<T> : ViewModelBase
+    {
+        private ObservableCollection<T> _Source = new ObservableCollection<T>();
+
+        /// <summary>
+        /// Gets the items collection for the current instance
+        /// </summary>
+        public ObservableCollection<T> Source
+        {
+            get => _Source;
+            protected set
+            {
+                // Update the source and the dependent properties
+                if (Set(ref _Source, value))
+                {
+                    RaisePropertyChanged(nameof(IsEmpty));
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets whether or not the current source collection is empty
+        /// </summary>
+        public bool IsEmpty => Source.Count == 0;
+
+        /// <summary>
+        /// Clears the collection of items, if possible
+        /// </summary>
+        /// <returns><see langword="true"/> if the collection was cleared, <see langword="false"/> if it was already empty</returns>
+        protected bool Clear()
+        {
+            if (IsEmpty) return false;
+
+            Source.Clear();
+            return true;
+        }
+
+        /// <inheritdoc cref="ViewModelBase.Cleanup"/>
+        public override void Cleanup()
+        {
+            base.Cleanup();
+
+            Source.Clear();
+            Source = null!;
+        }
+    }
+}
