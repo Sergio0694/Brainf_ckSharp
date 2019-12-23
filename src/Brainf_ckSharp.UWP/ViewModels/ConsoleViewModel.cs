81d296eb6b4542789a9fd309f95ab4db004a489c
Sergio Pedri <sergio0694@live.com>
2019-12-23 19:26:35 +0100

    Initial view and view model setup for the console

---
+++ src/Brainf_ckSharp.UWP/ViewModels/ConsoleViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Brainf_ckSharp.Interfaces;
+using Brainf_ckSharp.Tools;
+using Brainf_ckSharp.UWP.Models.Console;
+using Brainf_ckSharp.UWP.Models.Console.Interfaces;
+using Brainf_ckSharp.UWP.ViewModels.Abstract;
+
+namespace Brainf_ckSharp.UWP.ViewModels
+{
+    /// <summary>
+    /// A view model for an interactive REPL console for Brainf*ck/PBrain
+    /// </summary>
+    public sealed class ConsoleViewModel : ItemsCollectionViewModelBase<IConsoleEntry>
+    {
+        /// <summary>
+        /// Creates a new <see cref="ConsoleViewModel"/> instances with a new command ready to use
+        /// </summary>
+        public ConsoleViewModel()
+        {
+            Source.Add(new ConsoleCommand());
+        }
+
+        /// <summary>
+        /// Gets the <see cref="IReadOnlyTuringMachineState"/> instance currently in use
+        /// </summary>
+        public IReadOnlyTuringMachineState MachineState { get; private set; } = TuringMachineStateProvider.Default;
+    }
+}
---
+++ src/Brainf_ckSharp.UWP/Views/ConsoleView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Windows.UI.Xaml.Controls;
+using Brainf_ckSharp.UWP.ViewModels;
+
+#nullable enable
+
+namespace Brainf_ckSharp.UWP.Views
+{
+    /// <summary>
+    /// A view for an interactive REPL console for Brainf*ck/PBrain
+    /// </summary>
+    public sealed partial class ConsoleView : UserControl
+    {
+        public ConsoleView()
+        {
+            this.InitializeComponent();
+            this.DataContext = new ConsoleViewModel();
+        }
+
+        /// <summary>
+        /// Gets the <see cref="ConsoleViewModel"/> instance for the current view
+        /// </summary>
+        public ConsoleViewModel? ViewModel => this.DataContext as ConsoleViewModel;
+    }
+}
