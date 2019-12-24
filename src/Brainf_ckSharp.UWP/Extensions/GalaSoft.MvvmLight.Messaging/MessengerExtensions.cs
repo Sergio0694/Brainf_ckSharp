14ba550a855e2af27cf13769df4adf8d6a1ea3f2
Sergio Pedri <sergio0694@live.com>
2019-12-24 17:28:17 +0100

    Added request message support

--- src/Brainf_ckSharp.UWP/Extensions/GalaSoft.MvvmLight.Messaging/MessengerExtensions.cs
+++ src/Brainf_ckSharp.UWP/Extensions/GalaSoft.MvvmLight.Messaging/MessengerExtensions.cs
@@ -1,4 +1,7 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using Brainf_ckSharp.UWP.Messages.Abstract;
 
 namespace GalaSoft.MvvmLight.Messaging
 {
@@ -7,6 +10,23 @@ namespace GalaSoft.MvvmLight.Messaging
     /// </summary>
     public static class MessengerExtensions
     {
+        /// <summary>
+        /// Requests a result of a given type, using a specified message
+        /// </summary>
+        /// <typeparam name="TMessage">The type of message to send</typeparam>
+        /// <typeparam name="TResult">The returned type</typeparam>
+        /// <param name="messenger">The <see cref="IMessenger"/> instance to use</param>
+        [Pure]
+        public static TResult Request<TMessage, TResult>(this IMessenger messenger) where TMessage : RequestMessageBase<TResult>, new()
+        {
+            TMessage message = new TMessage();
+            messenger.Send(message);
+
+            if (!message.ResponseReceived) throw new InvalidOperationException("No response was received for the message");
+
+            return message.Result;
+        }
+
         /// <summary>
         /// Sends a message of a specified type by creating an instance with the default constructor
         /// </summary>
