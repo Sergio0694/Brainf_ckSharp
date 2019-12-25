d203ca3404af022c50f96fa5920249fc3eaaa473
Sergio Pedri <sergio0694@live.com>
2019-12-25 20:47:15 +0100

    Added draft for memory cell chunk template

---
+++ src/Brainf_ckSharp.UWP/Controls/DataTemplates/Brainf_ckMemoryCellChunkTemplate.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Windows.UI.Xaml.Controls;
+using Brainf_ckSharp.UWP.Models.Console.Controls;
+
+namespace Brainf_ckSharp.UWP.Controls.DataTemplates
+{
+    /// <summary>
+    /// A template to display a <see cref="Brainf_ckMemoryCellChunk"/> model
+    /// </summary>
+    public sealed partial class Brainf_ckMemoryCellChunkTemplate : UserControl
+    {
+        public Brainf_ckMemoryCellChunkTemplate()
+        {
+            this.InitializeComponent();
+            this.DataContextChanged += (s, e) => this.Bindings.Update();
+        }
+
+        /// <summary>
+        /// Gets the <see cref="Brainf_ckMemoryCellChunk"/> instance for the current view
+        /// </summary>
+        public Brainf_ckMemoryCellChunk ViewModel => this.DataContext as Brainf_ckMemoryCellChunk;
+    }
+}
