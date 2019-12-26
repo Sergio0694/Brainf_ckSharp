092d465c7e26cedbce5bd9912856e7cb00fc7011
Sergio Pedri <sergio0694@live.com>
2019-12-27 00:11:00 +0100

    Added the breakpoints guide section

---
+++ src/Brainf_ckSharp.UWP/Controls/SubPages/Shell/UserGuide/CodeSamples/BreakpointCodePreview.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Brainf_ckSharp.UWP.Controls.SubPages.Shell.UserGuide.CodeSamples
+{
+    /// <summary>
+    /// A control that displays a code sample with the use of a breakpoint
+    /// </summary>
+    public sealed partial class BreakpointCodePreview : UserControl
+    {
+        /// <summary>
+        /// Creates a new <see cref="BreakpointCodePreview"/>
+        /// </summary>
+        public BreakpointCodePreview()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ src/Brainf_ckSharp.UWP/Controls/SubPages/Shell/UserGuide/Sections/BreakpointGuidePanel.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Brainf_ckSharp.UWP.Controls.SubPages.Shell.UserGuide.Sections
+{
+    /// <summary>
+    /// A panel that displays info on the use of breakpoints to debug code
+    /// </summary>
+    public sealed partial class BreakpointGuidePanel : UserControl
+    {
+        /// <summary>
+        /// Creates a new <see cref="BreakpointGuidePanel"/>
+        /// </summary>
+        public BreakpointGuidePanel()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
