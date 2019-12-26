1ad7a4e29ce603c743b049a899616e144baf8acc
Sergio Pedri <sergio0694@live.com>
2019-12-26 20:28:25 +0100

    Added GitHub project

---
+++ src/GitHub/Models/User.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+#nullable disable
+
+namespace GitHub.Models
+{
+    /// <summary>
+    /// A model for a GitHub user
+    /// </summary>
+    public sealed class User
+    {
+        /// <summary>
+        /// Gets the name of the current contributor
+        /// </summary>
+        [JsonProperty("name")]
+        public string Name { get; internal set; }
+
+        /// <summary>
+        /// Gets the URL of the contributor profile image
+        /// </summary>
+        [JsonProperty("avatar_url")]
+        public string ProfileImageUrl { get; internal set; }
+
+        /// <summary>
+        /// Gets the URL of the contributor profile page
+        /// </summary>
+        [JsonProperty("html_url")]
+        public string ProfilePageUrl { get; internal set; }
+
+        /// <summary>
+        /// Gets the user bio
+        /// </summary>
+        [JsonProperty("bio")]
+        public string Bio { get; internal set; }
+    }
+}
