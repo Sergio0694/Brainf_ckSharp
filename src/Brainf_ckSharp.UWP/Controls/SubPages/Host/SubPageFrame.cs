01890d4b38ce63672757dfffacb7c0a7ca96f71b
Sergio Pedri <sergio0694@live.com>
2019-12-26 23:41:54 +0100

    Added sub page frame

---
+++ src/Brainf_ckSharp.UWP/Controls/SubPages/Host/SubPageFrame.cs
@@ -0,0 +1,31 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+#nullable enable
+
+namespace Brainf_ckSharp.UWP.Controls.SubPages.Host
+{
+    /// <summary>
+    /// A <see cref="ContentControl"/> that acts as a frame for a sub page
+    /// </summary>
+    public sealed class SubPageFrame : ContentControl
+    {
+        /// <summary>
+        /// Gets or sets the title for the current frame
+        /// </summary>
+        public string Title
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+
+        /// <summary>
+        /// The dependency property for <see cref="Title"/>
+        /// </summary>
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
+            nameof(Title),
+            typeof(string),
+            typeof(SubPageFrame),
+            new PropertyMetadata(string.Empty));
+    }
+}
