4394525dad649d42e5bcaab0fa6038cc561837e6
Sergio Pedri <sergio0694@live.com>
2019-12-27 00:18:37 +0100

    Added code samples guide panel

---
+++ src/Brainf_ckSharp.UWP/Controls/SubPages/Shell/UserGuide/Sections/CodeSamplesGuidePanel.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Brainf_ckSharp.UWP.Controls.SubPages.Shell.UserGuide.Sections
+{
+    /// <summary>
+    /// A panel that displays some code examples with step by step explanation
+    /// </summary>
+    public sealed partial class CodeSamplesGuidePanel : UserControl
+    {
+        /// <summary>
+        /// Creates a new <see cref="CodeSamplesGuidePanel"/>
+        /// </summary>
+        public CodeSamplesGuidePanel()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
