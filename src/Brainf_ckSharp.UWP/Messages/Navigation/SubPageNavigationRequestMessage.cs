2add4e05d587aff26375620cd38129c2a691e05f
Sergio Pedri <sergio0694@live.com>
2019-12-26 16:38:12 +0100

    Added SubPageHost control and related code

---
+++ src/Brainf_ckSharp.UWP/Messages/Navigation/SubPageNavigationRequestMessage.cs
@@ -0,0 +1,33 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Brainf_ckSharp.UWP.Messages.Navigation
+{
+    /// <summary>
+    /// A message that signals a request to display a page in the sub page host
+    /// </summary>
+    public sealed class SubPageNavigationRequestMessage
+    {
+        /// <summary>
+        /// Gets the page to display
+        /// </summary>
+        public UserControl SubPage { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="SubPageNavigationRequestMessage"/> instance with the specified parameters
+        /// </summary>
+        /// <param name="subPage">The page to display</param>
+        private SubPageNavigationRequestMessage(UserControl subPage) => SubPage = subPage;
+
+        /// <summary>
+        /// Creates a new request message to the target sub page type
+        /// </summary>
+        /// <param name="subPage">The page to display</param>
+        public static SubPageNavigationRequestMessage To(UserControl subPage) => new SubPageNavigationRequestMessage(subPage);
+
+        /// <summary>
+        /// Creates a new request message to the target sub page type
+        /// </summary>
+        /// <typeparam name="T">The type of page to display</typeparam>
+        public static SubPageNavigationRequestMessage To<T>() where T : UserControl, new() => new SubPageNavigationRequestMessage(new T());
+    }
+}
