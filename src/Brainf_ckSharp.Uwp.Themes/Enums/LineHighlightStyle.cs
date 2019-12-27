766e0cfc0711f8040231a692fdd2486be97f11c1
Sergio Pedri <sergio0694@live.com>
2019-12-27 19:48:58 +0100

    Added themes project

---
+++ src/Brainf_ckSharp.Uwp.Themes/Enums/LineHighlightStyle.cs
@@ -0,0 +1,18 @@
+﻿namespace Brainf_ckSharp.Uwp.Themes.Enums
+{
+    /// <summary>
+    /// An <see langword="enum"/> that indicates the type of line highlight for a given theme
+    /// </summary>
+    public enum LineHighlightStyle
+    {
+        /// <summary>
+        /// Indicates a rectangular outline around the current line
+        /// </summary>
+        Outline,
+
+        /// <summary>
+        /// Indicates a uniform fill with a given brush for the current line
+        /// </summary>
+        Fill
+    }
+}
