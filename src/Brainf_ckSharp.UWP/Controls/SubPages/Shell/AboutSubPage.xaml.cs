2f79181ba4c8c8aad99bd4dfbb4dfbcfe8ebcc7c
Sergio Pedri <sergio0694@live.com>
2019-12-27 11:11:23 +0100

    Added initial draft of the user guide sub page

--- src/Brainf_ckSharp.UWP/Controls/SubPages/Shell/AboutSubPage.xaml.cs
+++ src/Brainf_ckSharp.UWP/Controls/SubPages/Shell/AboutSubPage.xaml.cs
@@ -7,8 +7,14 @@ using Legere.ViewModels.SubPages.Shell;
 
 namespace Brainf_ckSharp.UWP.Controls.SubPages.Shell
 {
+    /// <summary>
+    /// A sub page that displays general info on the app
+    /// </summary>
     public sealed partial class AboutSubPage : UserControl, IConstrainedSubPage
     {
+        /// <summary>
+        /// Creates a new <see cref="AboutSubPage"/> instance
+        /// </summary>
         public AboutSubPage()
         {
             this.InitializeComponent();
---
+++ src/Brainf_ckSharp.UWP/Controls/SubPages/Shell/UserGuide/UserGuideSubPage.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Windows.UI.Xaml.Controls;
+using Brainf_ckSharp.UWP.Controls.SubPages.Interfaces;
+using Brainf_ckSharp.UWP.ViewModels.Controls.SubPages;
+
+#nullable enable
+
+namespace Brainf_ckSharp.UWP.Controls.SubPages.Shell.UserGuide
+{
+    /// <summary>
+    /// A sub page that displays the user guide for the app
+    /// </summary>
+    public sealed partial class UserGuideSubPage : UserControl, IConstrainedSubPage
+    {
+        /// <summary>
+        /// Creates a new <see cref="UserGuideSubPage"/> instance
+        /// </summary>
+        public UserGuideSubPage()
+        {
+            this.InitializeComponent();
+            this.DataContext = new UserGuideSubPageViewModel();
+        }
+
+        /// <summary>
+        /// Gets the <see cref="UserGuideSubPageViewModel"/> instance for the current view
+        /// </summary>
+        public UserGuideSubPageViewModel? ViewModel => this.DataContext as UserGuideSubPageViewModel;
+
+        /// <inheritdoc/>
+        public double MaxExpandedWidth { get; } = 400;
+
+        /// <inheritdoc/>
+        public double MaxExpandedHeight { get; } = 800;
+    }
+}
