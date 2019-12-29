9e993391713c583e1e5e3c1f90f09c254374fbb6
Sergio Pedri <sergio0694@live.com>
2019-12-29 23:23:40 +0100

    Added automatic scrolling on selection change

--- src/Brainf_ckSharp.UWP.Controls.IDE/Brainf_ckEditBox/Brainf_ckEditBox.cs
+++ src/Brainf_ckSharp.UWP.Controls.IDE/Brainf_ckEditBox/Brainf_ckEditBox.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using Windows.Foundation;
 using Windows.System;
+using Windows.UI.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Input;
@@ -19,12 +21,68 @@ namespace Brainf_ckSharp.Uwp.Controls.Ide
         public Brainf_ckEditBox()
         {
             SelectionChanging += Brainf_ckEditBox_SelectionChanging;
+            SelectionChanged += Brainf_ckEditBox_SelectionChanged;
             TextChanging += MarkdownRichEditBox_TextChanging;
             TextChanged += MarkdownRichEditBox_TextChanged;
             Paste += Brainf_ckEditBox_Paste;
         }
 
-        // Handles text being pasted by the user
+        /// <summary>
+        /// Updates <see cref="_SelectionLength"/> with the length of the current selection
+        /// </summary>
+        /// <param name="sender">The current <see cref="Brainf_ckEditBox"/> instance</param>
+        /// <param name="args">The <see cref="RichEditBoxSelectionChangingEventArgs"/> instance for the <see cref="RichEditBox.SelectionChanging"/> event</param>
+        private void Brainf_ckEditBox_SelectionChanging(RichEditBox sender, RichEditBoxSelectionChangingEventArgs args) => _SelectionLength = args.SelectionLength;
+
+        /// <summary>
+        /// Adjusts the scrolling offset on both axes when the selection changes
+        /// </summary>
+        /// <param name="sender">The current <see cref="Brainf_ckEditBox"/> instance</param>
+        /// <param name="e">The <see cref="RoutedEventArgs"/> instance for the <see cref="RichEditBox.SelectionChanged"/> event</param>
+        private void Brainf_ckEditBox_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            Document.Selection.GetRect(PointOptions.Transform, out Rect rect, out _);
+
+            double
+                verticalOffset = _ContentScroller.VerticalOffset,
+                horizontalOffset = _ContentScroller.HorizontalOffset,
+                viewportHeight = _ContentScroller.ViewportHeight - VerticalScrollBarMargin.Top,
+                viewportWidth = _ContentScroller.ViewportWidth,
+                transformedVerticalOffset = rect.Top - verticalOffset;
+
+            const double NegativeLeftOffsetBeforeSelection = 12;
+            const double HorizontalScrollingThreshold = 20;
+            const double RightOffsetAfterSelection = 32;
+
+            // Calculate the target horizontal offset
+            double? horizontal;
+            if (rect.Left < horizontalOffset) horizontal = rect.Left - NegativeLeftOffsetBeforeSelection;
+            else if (rect.Left > viewportWidth + horizontalOffset - HorizontalScrollingThreshold)
+            {
+                horizontal = rect.Left - viewportWidth + RightOffsetAfterSelection;
+            }
+            else horizontal = null;
+
+            const double VerticalScrollingThreshold = 32;
+            const double BottomOffsetBelowSelection = 32;
+
+            // Calculate the target vertical offset
+            double? vertical;
+            if (transformedVerticalOffset < 0) vertical = verticalOffset + transformedVerticalOffset;
+            else if (transformedVerticalOffset > viewportHeight - VerticalScrollingThreshold)
+            {
+                vertical = verticalOffset + (transformedVerticalOffset - viewportHeight) + BottomOffsetBelowSelection;
+            }
+            else vertical = null;
+
+            _ContentScroller.ChangeView(horizontal, vertical, null, false);
+        }
+
+        /// <summary>
+        /// Retrieves, inserts and formats text being pasted by the user
+        /// </summary>
+        /// <param name="sender">The current <see cref="Brainf_ckEditBox"/> instance</param>
+        /// <param name="e">The <see cref="TextControlPasteEventArgs"/> instance for the <see cref="RichEditBox.Paste"/> event</param>
         private async void Brainf_ckEditBox_Paste(object sender, TextControlPasteEventArgs e)
         {
             e.Handled = true;
@@ -34,12 +92,6 @@ namespace Brainf_ckSharp.Uwp.Controls.Ide
             {
                 InsertText(text);
             }
-        }
-
-        // Updates the length of the current selection
-        private void Brainf_ckEditBox_SelectionChanging(RichEditBox sender, RichEditBoxSelectionChangingEventArgs args)
-        {
-            _SelectionLength = args.SelectionLength;
         }
 
         // Checks when the text changes and applies the syntax highlight
