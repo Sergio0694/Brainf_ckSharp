58f76708f9afdcba053e356ce92b6b1aa3efbb59
Sergio Pedri <sergio0694@live.com>
2019-12-29 15:24:21 +0100

    Added loading of column guides info

--- src/Brainf_ckSharp.Uwp.Controls.Ide/Brainf_ckEditBox/Brainf_ckEditBox.Win2D.cs
+++ src/Brainf_ckSharp.Uwp.Controls.Ide/Brainf_ckEditBox/Brainf_ckEditBox.Win2D.cs
@@ -2,9 +2,12 @@
 using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using Windows.Foundation;
+using Windows.UI.Text;
 using Brainf_ckSharp.Buffers;
 using Brainf_ckSharp.Constants;
 using Brainf_ckSharp.Helpers;
+using Brainf_ckSharp.Uwp.Controls.Ide.Models;
 
 #nullable enable
 
@@ -13,9 +16,14 @@ namespace Brainf_ckSharp.Uwp.Controls.Ide
     public sealed partial class Brainf_ckEditBox
     {
         /// <summary>
-        /// The current sequence of brackets being displayed in the text
+        /// The current sequence of bracket pairs being displayed in the text
         /// </summary>
-        private MemoryOwner<(int Start, int End)> _BracketsList = MemoryOwner<(int, int)>.Allocate(0);
+        private MemoryOwner<(int Start, int End)> _BracketPairs = MemoryOwner<(int, int)>.Allocate(0);
+
+        /// <summary>
+        /// The current sequence of column guide coordinates to render
+        /// </summary>
+        private MemoryOwner<ColumnGuideInfo> _ColumnGuides = MemoryOwner<ColumnGuideInfo>.Allocate(0);
 
         /// <summary>
         /// Tries to update the current sequence of brackets displayed in the text
@@ -88,17 +96,54 @@ namespace Brainf_ckSharp.Uwp.Controls.Ide
             }
 
             // Skip the update if both sequences match
-            if (_BracketsList.AsBytes().SequenceEqual(jumpTable.AsBytes()))
+            if (_BracketPairs.AsBytes().SequenceEqual(jumpTable.AsBytes()))
             {
                 jumpTable.Dispose();
                 return false;
             }
 
             // Update the current brackets sequence
-            _BracketsList.Dispose();
-            _BracketsList = jumpTable.Slice(jumps);
+            _BracketPairs.Dispose();
+            _BracketPairs = jumpTable.Slice(jumps);
 
             return true;
+        }
+
+        /// <summary>
+        /// Processes the current column guides info and updates <see cref="_ColumnGuides"/>
+        /// </summary>
+        private void ProcessColumnGuides()
+        {
+            _ColumnGuides.Dispose();
+
+            // Update the target buffer
+            MemoryOwner<(int, int)> bracketPairs = _BracketPairs;
+            MemoryOwner<ColumnGuideInfo> columnGuides = MemoryOwner<ColumnGuideInfo>.Allocate(bracketPairs.Size);
+
+            _ColumnGuides = columnGuides;
+
+            // Skip if there are no brackets to render
+            if (columnGuides.Size == 0) return;
+
+            ref (int Start, int End) bracketPairsRef = ref bracketPairs.GetReference();
+            ref ColumnGuideInfo columnGuidesRef = ref columnGuides.GetReference();
+
+            for (int i = 0; i < bracketPairs.Size; i++)
+            {
+                // Get the initial and ending range
+                var bounds = Unsafe.Add(ref bracketPairsRef, i);
+                ITextRange range = Document.GetRangeAt(bounds.Start);
+                range.GetRect(PointOptions.Transform, out Rect open, out _);
+                range = Document.GetRangeAt(bounds.End);
+                range.GetRect(PointOptions.Transform, out Rect close, out _);
+
+                // Render the new line guide
+                ColumnGuideInfo guideInfo = new ColumnGuideInfo(
+                    MathF.Min((float)open.X, (float)close.X),
+                    (float)open.Top,
+                    (float)(close.Top - open.Bottom));
+                Unsafe.Add(ref columnGuidesRef, i) = guideInfo;
+            }
         }
     }
 }
