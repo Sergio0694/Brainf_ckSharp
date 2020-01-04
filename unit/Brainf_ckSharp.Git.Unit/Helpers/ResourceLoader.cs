8893829479a5bf3b99d83f48cb491c8b4f9a47fc
Sergio Pedri <sergio0694@live.com>
2020-01-04 17:19:05 +0100

    Added empty .Git unit test project

---
+++ unit/Brainf_ckSharp.Git.Unit/Helpers/ResourceLoader.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics.Contracts;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+
+namespace Brainf_ckSharp.Git.Unit.Helpers
+{
+    /// <summary>
+    /// A <see langword="class"/> with helper methods to load resource files
+    /// </summary>
+    internal sealed class ResourceLoader
+    {
+        /// <summary>
+        /// Loads a pair of test samples with the specified key
+        /// </summary>
+        /// <param name="key">The key of the test samples to retrieve</param>
+        /// <returns>A pair of reference and updated text files with the specified key</returns>
+        [Pure]
+        public static (string Old, string New) LoadTestSample(string key)
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+
+            string
+                oldPath = assembly.GetManifestResourceNames().First(name => name.EndsWith($"{key}Old.txt")),
+                newPath = assembly.GetManifestResourceNames().First(name => name.EndsWith($"{key}New.txt"));
+
+            using Stream oldStream = assembly.GetManifestResourceStream(oldPath);
+            using StreamReader oldReader = new StreamReader(oldStream);
+
+            using Stream newStream = assembly.GetManifestResourceStream(newPath);
+            using StreamReader newReader = new StreamReader(newStream);
+
+            string
+                oldText = oldReader.ReadToEnd().Replace("\n", string.Empty),
+                newText = newReader.ReadToEnd().Replace("\n", string.Empty);
+
+            return (oldText, newText);
+        }
+    }
+}
