7a5d9c9b3b8fc21c8f69e47166754e6d177974c2
Sergio Pedri <sergio0694@live.com>
2020-01-04 16:02:22 +0100

    Added .Git project and initial extensions

---
+++ src/Brainf_ckSharp.Git/Extensions/ReadOnlySpanExtensions.cs
@@ -0,0 +1,55 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace System
+{
+    /// <summary>
+    /// A <see langword="class"/> with some extension methods for the <see cref="ReadOnlySpan{T}"/> type
+    /// </summary>
+    public static class ReadOnlySpanExtensions
+    {
+        /// <summary>
+        /// Gets a content hash from the input <see cref="ReadOnlySpan{T}"/> instance using the xxHash32 algorithm
+        /// </summary>
+        /// <typeparam name="T">The type of items in the input <see cref="ReadOnlySpan{T}"/> instance</typeparam>
+        /// <param name="span">The input <see cref="ReadOnlySpan{T}"/> instance</param>
+        /// <returns>The xxHash32 value for the input <see cref="ReadOnlySpan{T}"/> instance</returns>
+        [Pure]
+        public static int GetxxHash32Code<T>(this ReadOnlySpan<T> span)
+        {
+            int length = span.Length;
+
+            // Fallback if the span is empty
+            if (length == 0) return 0;
+
+            ref T r0 = ref MemoryMarshal.GetReference(span);
+            int
+                hash = 0,
+                end = length - 8,
+                i = 0;
+
+            // Vectorize with as many items at once as possible
+            for (; i < end; i += 7)
+            {
+                hash = HashCode.Combine(
+                    hash,
+                    Unsafe.Add(ref r0, i),
+                    Unsafe.Add(ref r0, i + 1),
+                    Unsafe.Add(ref r0, i + 2),
+                    Unsafe.Add(ref r0, i + 3),
+                    Unsafe.Add(ref r0, i + 4),
+                    Unsafe.Add(ref r0, i + 5),
+                    Unsafe.Add(ref r0, i + 6));
+            }
+
+            // Handle the leftover items
+            for (; i < length; i++)
+            {
+                hash = HashCode.Combine(hash, Unsafe.Add(ref r0, i));
+            }
+
+            return hash;
+        }
+    }
+}
---
+++ src/Brainf_ckSharp.Git/Extensions/StringExtensions.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+namespace System
+{
+    /// <summary>
+    /// A <see langword="class"/> with some extension methods for the <see cref="string"/> type
+    /// </summary>
+    internal static class StringExtensions
+    {
+        /// <summary>
+        /// Counts the number of occurrences of a given character into a target <see cref="string"/>
+        /// </summary>
+        /// <param name="text">The input text to read</param>
+        /// <param name="c">The character to look for</param>
+        /// <returns>The number of occurrences of <paramref name="c"/> in <paramref name="text"/></returns>
+        [Pure]
+        public static int Count(this string text, char c)
+        {
+            int length = text.Length;
+
+            // Empty string, just return 0
+            if (length == 0) return 0;
+
+            ref char r0 = ref MemoryMarshal.GetReference(text.AsSpan());
+            int result = 0;
+
+            // Go over the input text and look for the character
+            for (int i = 0; i < length; i++)
+                if (Unsafe.Add(ref r0, i) == c)
+                    result++;
+
+            return result;
+        }
+    }
+}
