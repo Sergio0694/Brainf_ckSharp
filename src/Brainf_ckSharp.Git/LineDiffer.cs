c1dba1bedffbef85650c69f85a89b16a44a6fcca
Sergio Pedri <sergio0694@live.com>
2020-01-04 16:39:13 +0100

    Minor code refactoring

--- src/Brainf_ckSharp.Git/LineDiffer.cs
+++ src/Brainf_ckSharp.Git/LineDiffer.cs
@@ -3,6 +3,7 @@ using System.Buffers;
 using System.Diagnostics.Contracts;
 using System.Runtime.CompilerServices;
 using Brainf_ckSharp.Git.Enums;
+using Brainf_ckSharp.Git.Models;
 using Microsoft.Collections.Extensions;
 
 namespace Brainf_ckSharp.Git
@@ -15,7 +16,7 @@ namespace Brainf_ckSharp.Git
         /// <summary>
         /// The reusable <see cref="DictionarySlim{TKey,TValue}"/>
         /// </summary>
-        private static readonly DictionarySlim<int, Entry> LinesMap = new DictionarySlim<int, Entry>();
+        private static readonly DictionarySlim<int, DiffEntry> LinesMap = new DictionarySlim<int, DiffEntry>();
 
         /// <summary>
         /// Computes the line difference for a reference text and a new text
@@ -32,10 +33,10 @@ namespace Brainf_ckSharp.Git
             object[] OA = ArrayPool<object>.Shared.Rent(OL);
             object[] NA = ArrayPool<object>.Shared.Rent(NL);
 
-            DictionarySlim<int, Entry> table = LinesMap;
+            DictionarySlim<int, DiffEntry> table = LinesMap;
             table.Clear();
 
-            Pool<Entry>.Reset();
+            Pool<DiffEntry>.Reset();
 
             try
             {
@@ -44,19 +45,19 @@ namespace Brainf_ckSharp.Git
                 foreach (ReadOnlySpan<char> line in newText.Tokenize(separator))
                 {
                     int hash = line.GetxxHash32Code();
-                    ref Entry entry = ref table.GetOrAddValueRef(hash);
+                    ref DiffEntry entry = ref table.GetOrAddValueRef(hash);
 
                     if (entry is null)
                     {
-                        entry = Pool<Entry>.Rent();
-                        entry.NC = 1;
-                        entry.OC = 0;
-                        entry.OLNO = 0;
+                        entry = Pool<DiffEntry>.Rent();
+                        entry.NumberOfOccurrencesInNewText = 1;
+                        entry.NumberOfOccurrencesInOldText = 0;
+                        entry.LineNumberInOldText = 0;
                     }
                     else
                     {
-                        if (entry.NC == 1) entry.NC = 2;
-                        else entry.NC = int.MaxValue;
+                        if (entry.NumberOfOccurrencesInNewText == 1) entry.NumberOfOccurrencesInNewText = 2;
+                        else entry.NumberOfOccurrencesInNewText = int.MaxValue;
                     }
 
                     NA[i] = entry;
@@ -68,23 +69,23 @@ namespace Brainf_ckSharp.Git
                 foreach (ReadOnlySpan<char> line in oldText.Tokenize(separator))
                 {
                     int hash = line.GetxxHash32Code();
-                    ref Entry entry = ref table.GetOrAddValueRef(hash);
+                    ref DiffEntry entry = ref table.GetOrAddValueRef(hash);
 
                     if (entry is null)
                     {
-                        entry = Pool<Entry>.Rent();
-                        entry.NC = 0;
-                        entry.OC = 1;
-                        entry.OLNO = 0;
+                        entry = Pool<DiffEntry>.Rent();
+                        entry.NumberOfOccurrencesInNewText = 0;
+                        entry.NumberOfOccurrencesInOldText = 1;
+                        entry.LineNumberInOldText = 0;
                     }
                     else
                     {
-                        if (entry.OC == 0) entry.OC = 1;
-                        else if (entry.OC == 1) entry.OC = 2;
-                        else entry.OC = int.MaxValue;
+                        if (entry.NumberOfOccurrencesInOldText == 0) entry.NumberOfOccurrencesInOldText = 1;
+                        else if (entry.NumberOfOccurrencesInOldText == 1) entry.NumberOfOccurrencesInOldText = 2;
+                        else entry.NumberOfOccurrencesInOldText = int.MaxValue;
                     }
 
-                    entry.OLNO = j;
+                    entry.LineNumberInOldText = j;
                     OA[j] = entry;
                     j += 1;
                 }
@@ -93,11 +94,11 @@ namespace Brainf_ckSharp.Git
                 i = 0;
                 for (; i < NL; i++)
                 {
-                    if (NA[i] is Entry entry &&
-                        entry.OC == 1 &&
-                        entry.NC == 1)
+                    if (NA[i] is DiffEntry entry &&
+                        entry.NumberOfOccurrencesInOldText == 1 &&
+                        entry.NumberOfOccurrencesInNewText == 1)
                     {
-                        int olno = entry.OLNO;
+                        int olno = entry.LineNumberInOldText;
                         NA[i] = olno;
                         OA[olno] = i;
                     }
@@ -142,18 +143,6 @@ namespace Brainf_ckSharp.Git
             {
                 ArrayPool<object>.Shared.Return(OA);
                 ArrayPool<object>.Shared.Return(NA);
-            }
-        }
-
-        public sealed class Entry
-        {
-            public int OC;
-            public int NC;
-            public int OLNO;
-
-            public override string ToString()
-            {
-                return $"OC: {OC}, NC: {NC}, OLNO: {OLNO}";
             }
         }
     }
