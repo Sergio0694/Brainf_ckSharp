02ed9ccf771de270cdfa6b95729b7def15f32e7d
Sergio Pedri <sergio0694@live.com>
2020-01-10 16:03:51 +0100

    Added git diff test for multiple blank lines

--- unit/Brainf_ckSharp.Git.Unit/DiffTest.cs
+++ unit/Brainf_ckSharp.Git.Unit/DiffTest.cs
@@ -100,6 +100,25 @@ namespace Brainf_ckSharp.Git.Unit
             Test(expected);
         }
 
+        [TestMethod]
+        public void Large()
+        {
+            LineModificationType[] expected = new LineModificationType[96];
+
+            foreach (int edit in new[]
+            {
+                10, 11, 12,
+                28, 29, 30, 31, 32,
+                66, 67, 68, 69, 70, 71,
+                87, 88
+            })
+            {
+                expected[edit] = LineModificationType.Modified;
+            }
+
+            Test(expected);
+        }
+
         /// <summary>
         /// Runs a test for a pair of resources with the specified name
         /// </summary>
