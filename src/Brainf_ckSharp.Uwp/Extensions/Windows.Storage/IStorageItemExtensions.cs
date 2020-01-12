ab210e10975bed90f050611a31233e6fe1c420da
Sergio Pedri <sergio0694@live.com>
2020-01-12 23:45:21 +0100

    Added loading of formatted paths for recent files

--- src/Brainf_ckSharp.Uwp/Extensions/Windows.Storage/IStorageItemExtensions.cs
+++ src/Brainf_ckSharp.Uwp/Extensions/Windows.Storage/IStorageItemExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Windows.ApplicationModel;
+﻿using System;
+using System.IO;
+using Windows.ApplicationModel;
 using Windows.Storage;
 using JetBrains.Annotations;
 
@@ -18,6 +20,46 @@ namespace Brainf_ckSharp.Uwp.Extensions.Windows.Storage
         public static bool IsFromPackageDirectory(this IStorageItem item)
         {
             return item.Path.StartsWith(Package.Current.InstalledLocation.Path);
+        }
+
+        /// <summary>
+        /// Gets a formatted path <see cref="string"/> with a " » " separator
+        /// </summary>
+        /// <param name="item">The input <see cref="IStorageItem"/> instance to analyze</param>
+        /// <returns>A formatted path <see cref="string"/> for <paramref name="item"/></returns>
+        [Pure]
+        public static unsafe string GetFormattedPath(this IStorageItem item)
+        {
+            const int separatorLength = 3; // " » "
+            int
+                numberOfSeparators = item.Path.Count(Path.DirectorySeparatorChar),
+                formattedLength = item.Path.Length + numberOfSeparators * (separatorLength - 1) + separatorLength;
+
+            /* The temporary buffer has space for one extra separator that is
+             * always initialized even if it's not used in the final string.
+             * This is done to avoid having to check the current index in the
+             * main loop, which would be needed to check whether or not
+             * the current separator should be written or not to the buffer. */
+            char* p = stackalloc char[formattedLength];
+
+            // Write the path parts
+            int i = 0;
+            foreach (ReadOnlySpan<char> part in item.Path.Tokenize(Path.DirectorySeparatorChar))
+            {
+                part.CopyTo(new Span<char>(p + i, part.Length));
+
+                i += part.Length;
+
+                // Write the characters manually to avoid another stackalloc
+                p[i] = ' ';
+                p[i + 1] = '»';
+                p[i + 2] = ' ';
+
+                i += 3;
+            }
+
+            // Create a string from the buffer and skip the last separator
+            return new string(p, 0, formattedLength - 3);
         }
     }
 }
