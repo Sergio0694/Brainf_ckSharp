eae1373a5b0c9ae897546924a849413e777b684d
Sergio Pedri <sergio0694@live.com>
2020-01-15 17:13:59 +0100

    Added initial unicode map templates

---
+++ src/Brainf_ckSharp.Uwp/Controls/SubPages/Views/CodeLibraryMap/Templates/UnicodeVisibleCharacterTemplate.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Brainf_ckSharp.Uwp.Bindings.Functions;
+using Brainf_ckSharp.Uwp.Models;
+
+#nullable enable
+
+namespace Brainf_ckSharp.Uwp.Controls.SubPages.Views.CodeLibraryMap.Templates
+{
+    public sealed partial class UnicodeVisibleCharacterTemplate : UserControl
+    {
+        public UnicodeVisibleCharacterTemplate()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets or sets the value to display for this characters group
+        /// </summary>
+        public UnicodeCharacter Character
+        {
+            get => (UnicodeCharacter)GetValue(CharacterProperty);
+            set => SetValue(CharacterProperty, value);
+        }
+
+        public static readonly DependencyProperty CharacterProperty = DependencyProperty.Register(
+            nameof(Character),
+            typeof(UnicodeCharacter),
+            typeof(UnicodeVisibleCharacterTemplate),
+            new PropertyMetadata(DependencyProperty.UnsetValue, OnCharacterPropertyChanged));
+
+        /// <summary>
+        /// Updates the <see cref="TextBlock.Text"/> property on <see cref="NumberBlock"/> and <see cref="ValueBlock"/> when <see cref="Character"/> changes
+        /// </summary>
+        /// <param name="d">The source <see cref="UnicodeVisibleCharacterTemplate"/> instance</param>
+        /// <param name="e">The <see cref="DependencyPropertyChangedEventArgs"/> instance with the new <see cref="Character"/> value</param>
+        private static void OnCharacterPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            UnicodeVisibleCharacterTemplate @this = (UnicodeVisibleCharacterTemplate)d;
+            UnicodeCharacter value = (UnicodeCharacter)e.NewValue;
+
+            @this.NumberBlock.Text = ((ushort)value.Value).ToString();
+            @this.ValueBlock.Text = NumericFunctions.ConvertToVisibleText(value.Value);
+        }
+    }
+}
---
+++ src/Brainf_ckSharp.Uwp/Controls/SubPages/Views/CodeLibraryMap/UnicodeCharactersMapSubPage.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Windows.UI.Xaml.Controls;
+using Brainf_ckSharp.Uwp.Controls.SubPages.Interfaces;
+using Brainf_ckSharp.Uwp.ViewModels.Controls.SubPages;
+
+#nullable enable
+
+namespace Brainf_ckSharp.Uwp.Controls.SubPages.Views.CodeLibraryMap
+{
+    public sealed partial class UnicodeCharactersMapSubPage : UserControl, IConstrainedSubPage
+    {
+        public UnicodeCharactersMapSubPage()
+        {
+            this.InitializeComponent();
+        }
+
+        /// <summary>
+        /// Gets the <see cref="CodeLibrarySubPageViewModel"/> instance for the current view
+        /// </summary>
+        public CodeLibrarySubPageViewModel? ViewModel => this.DataContext as CodeLibrarySubPageViewModel;
+
+        /// <inheritdoc/>
+        public double MaxExpandedWidth { get; } = 460;
+
+        /// <inheritdoc/>
+        public double MaxExpandedHeight { get; } = double.PositiveInfinity;
+    }
+}
