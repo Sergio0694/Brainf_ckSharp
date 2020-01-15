369dea0869d2bea4ab077e66e9a0ae2a49be6e3d
Sergio Pedri <sergio0694@live.com>
2020-01-15 22:43:34 +0100

    Added initial dotnet CLI tool

---
+++ src/Brainf_ckSharp.Cli/Options.cs
@@ -0,0 +1,80 @@
+﻿using Brainf_ckSharp.Enums;
+using CommandLine;
+
+namespace Brainf_ckSharp.Cli
+{
+    /// <summary>
+    /// A model with all the options for a script invocation
+    /// </summary>
+    public sealed class Options
+    {
+        /// <summary>
+        /// Gets or sets the source file with the code to execute
+        /// </summary>
+        [Option(
+            'f',
+            "file",
+            HelpText = "The source file with the code to execute",
+            SetName = "source")]
+        public string? SourceFile { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the source code to execute
+        /// </summary>
+        [Option(
+            's',
+            "source",
+            HelpText = "The source code to execute",
+            SetName = "source")]
+        public string? Source { get; set; }
+
+        /// <summary>
+        /// Gets or sets the stdin buffer to pass to the script
+        /// </summary>
+        [Option(
+            "in",
+            HelpText = "The stdin buffer to pass to the script",
+            Required = false)]
+        public string? Stdin { get; set; }
+
+        /// <summary>
+        /// Gets or sets the size of the memory buffer to use
+        /// </summary>
+        [Option(
+            "memory-size",
+            Default = 128,
+            HelpText = "The size of the memory buffer to use [32, 65536]",
+            Required = false)]
+        public int MemorySize { get; set; }
+
+        /// <summary>
+        /// Gets or sets the path for a file to dump the output buffer to
+        /// </summary>
+        [Option(
+            "out",
+            HelpText = "The path for a file to dump the output buffer to",
+            Required = false)]
+        public string? Stdout { get; set; }
+
+        /// <summary>
+        /// Gets or sets the timeout for the script to execute
+        /// </summary>
+        [Option(
+            't',
+            "timeout",
+            Default = 2,
+            HelpText = "The timeout in seconds for the script to execute (0 to disable)",
+            Required = false)]
+        public int Timeout { get; set; }
+        
+        /// <summary>
+        /// Gets or sets the overflow mode to use for the memory buffer
+        /// </summary>
+        [Option(
+            "overflow",
+            Default = OverflowMode.ByteWithOverflow,
+            HelpText = "The overflow mode to use for the memory buffer",
+            Required = false)]
+        public OverflowMode OverflowMode { get; set; }
+    }
+}
---
+++ src/Brainf_ckSharp.Cli/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using Brainf_ckSharp.Models;
+using Brainf_ckSharp.Models.Base;
+using CommandLine;
+
+namespace Brainf_ckSharp.Cli
+{
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ParserResult<Options> parserResult = Parser.Default.ParseArguments<Options>(args);
+
+            parserResult.WithParsed(options =>
+            {
+                // Get the source code to execute
+                string source;
+                if (options.SourceFile is { } && File.Exists(options.SourceFile))
+                {
+                    source = File.ReadAllText(options.SourceFile);
+                }
+                else source = options.Source!;
+
+                // Create the cancellation token source
+                CancellationTokenSource cts = options.Timeout == 0
+                    ? new CancellationTokenSource()
+                    : new CancellationTokenSource(TimeSpan.FromSeconds(options.Timeout));
+
+                // Execute the code
+                Option<InterpreterResult> interpreterResult = Brainf_ckInterpreter.TryRun(
+                    source,
+                    options.Stdin ?? string.Empty,
+                    options.MemorySize,
+                    options.OverflowMode,
+                    cts.Token);
+
+                // Display the execution result
+                if (interpreterResult.Value is { })
+                {
+                    Console.WriteLine(interpreterResult.Value.Stdout);
+
+                    if (options.Stdout is { }) File.WriteAllText(options.Stdout, interpreterResult.Value.Stdout);
+                }
+                else Console.WriteLine(interpreterResult.ValidationResult.ErrorType);
+            });
+
+            parserResult.WithNotParsed(errors =>
+            {
+                foreach (Error error in errors)
+                    Console.WriteLine(error);
+            });
+        }
+    }
+}
