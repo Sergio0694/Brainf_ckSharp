1d120949a7aec8883624478470618a456917f082
Sergio Pedri <sergio0694@live.com>
2020-03-04 18:48:35 +0100

    Code refactoring

--- src/Brainf_ckSharp.Uwp/Controls/SubPages/Views/CodeLibraryMap/Templates/UnicodeIntervalTemplate.xaml.cs
+++ src/Brainf_ckSharp.Uwp/Controls/SubPages/Views/CodeLibraryMap/Templates/UnicodeIntervalTemplate.xaml.cs
@@ -3,18 +3,18 @@ using Windows.UI.Xaml.Controls;
 
 namespace Brainf_ckSharp.Uwp.Controls.SubPages.Views.CodeLibraryMap.Templates
 {
-    public sealed partial class UnicodeNonVisibleCharactersGroupTemplate : UserControl
+    public sealed partial class UnicodeIntervalTemplate : UserControl
     {
         /// <summary>
-        /// Creates a new <see cref="UnicodeNonVisibleCharactersGroupTemplate"/> instance
+        /// Creates a new <see cref="UnicodeIntervalTemplate"/> instance
         /// </summary>
-        public UnicodeNonVisibleCharactersGroupTemplate()
+        public UnicodeIntervalTemplate()
         {
             this.InitializeComponent();
         }
 
         /// <summary>
-        /// Gets or sets the value to display for this characters group
+        /// Gets or sets the value to display for this characters interval
         /// </summary>
         public string Value
         {
@@ -25,24 +25,24 @@ namespace Brainf_ckSharp.Uwp.Controls.SubPages.Views.CodeLibraryMap.Templates
         public static readonly DependencyProperty ValueProperty = DependencyProperty.Register(
             nameof(Value),
             typeof(string),
-            typeof(UnicodeNonVisibleCharactersGroupTemplate),
+            typeof(UnicodeIntervalTemplate),
             new PropertyMetadata(string.Empty, OnValuePropertyChanged));
 
         /// <summary>
         /// Updates the <see cref="TextBlock.Text"/> property on <see cref="ValueBlock"/> when <see cref="Value"/> changes
         /// </summary>
-        /// <param name="d">The source <see cref="UnicodeNonVisibleCharactersGroupTemplate"/> instance</param>
+        /// <param name="d">The source <see cref="UnicodeIntervalTemplate"/> instance</param>
         /// <param name="e">The <see cref="DependencyPropertyChangedEventArgs"/> instance with the new <see cref="Value"/> value</param>
         private static void OnValuePropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            UnicodeNonVisibleCharactersGroupTemplate @this = (UnicodeNonVisibleCharactersGroupTemplate)d;
+            UnicodeIntervalTemplate @this = (UnicodeIntervalTemplate)d;
             string text = (string)e.NewValue;
 
             @this.ValueBlock.Text = text;
         }
 
         /// <summary>
-        /// Gets or sets the description to display for this characters group
+        /// Gets or sets the description to display for this characters interval
         /// </summary>
         public string Description
         {
@@ -53,17 +53,17 @@ namespace Brainf_ckSharp.Uwp.Controls.SubPages.Views.CodeLibraryMap.Templates
         public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
             nameof(Description),
                 typeof(string),
-                typeof(UnicodeNonVisibleCharactersGroupTemplate),
+                typeof(UnicodeIntervalTemplate),
             new PropertyMetadata(string.Empty, OnDescriptionPropertyPropertyChanged));
 
         /// <summary>
         /// Updates the <see cref="TextBlock.Text"/> property on <see cref="DescriptionBlock"/> when <see cref="Description"/> changes
         /// </summary>
-        /// <param name="d">The source <see cref="UnicodeNonVisibleCharactersGroupTemplate"/> instance</param>
+        /// <param name="d">The source <see cref="UnicodeIntervalTemplate"/> instance</param>
         /// <param name="e">The <see cref="DependencyPropertyChangedEventArgs"/> instance with the new <see cref="Description"/> value</param>
         private static void OnDescriptionPropertyPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            UnicodeNonVisibleCharactersGroupTemplate @this = (UnicodeNonVisibleCharactersGroupTemplate)d;
+            UnicodeIntervalTemplate @this = (UnicodeIntervalTemplate)d;
             string text = (string)e.NewValue;
 
             @this.DescriptionBlock.Text = text;
