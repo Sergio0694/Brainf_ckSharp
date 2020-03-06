e7653806f5456ad1fae8edc5886b4a280edaaabd
Sergio Pedri <sergio0694@live.com>
2020-03-06 22:24:14 +0100

    Added Brainf_ckOperation type

---
+++ src/Brainf_ckSharp/Models/Internal/Brainf_ckOperation.cs
@@ -0,0 +1,29 @@
+﻿namespace Brainf_ckSharp.Models.Internal
+{ 
+    /// <summary>
+    /// A model that represents an RLE-compressed operator
+    /// </summary>
+    internal readonly struct Brainf_ckOperation
+    {
+        /// <summary>
+        /// The operator to execute
+        /// </summary>
+        public readonly byte Operator;
+
+        /// <summary>
+        /// The number of times to repeat the operator
+        /// </summary>
+        public readonly int Count;
+
+        /// <summary>
+        /// Creates a new <see cref="Brainf_ckOperation"/> instance with the specified values
+        /// </summary>
+        /// <param name="op"></param>
+        /// <param name="count"></param>
+        public Brainf_ckOperation(byte op, int count)
+        {
+            Operator = op;
+            Count = count;
+        }
+    }
+}
