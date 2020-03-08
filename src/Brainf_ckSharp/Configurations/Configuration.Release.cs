02312bc9788b71d363f95a17c42ba0f8ff34aa09
Sergio Pedri <sergio0694@live.com>
2020-03-08 15:03:39 +0100

    Added configuration types

---
+++ src/Brainf_ckSharp/Configurations/Configuration.Release.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading;
+
+namespace Brainf_ckSharp.Configurations
+{
+    /// <summary>
+    /// A model for a RELEASE configuration being built
+    /// </summary>
+    public readonly ref partial struct ReleaseConfiguration
+    {
+        /// <summary>
+        /// The sequence of indices for the breakpoints to apply to the script
+        /// </summary>
+        public readonly ReadOnlySpan<int> Breakpoints;
+
+        /// <summary>
+        /// The token to cancel the monitoring of breakpoints
+        /// </summary>
+        public readonly CancellationToken DebugToken;
+    }
+}
