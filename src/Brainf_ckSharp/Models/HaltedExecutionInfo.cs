985c039655f92dacc1592ccf6dc2379b7bfefa80
Sergio Pedri <sergio0694@live.com>
2020-03-08 02:12:32 +0100

    Code style tweaks

--- src/Brainf_ckSharp/Models/HaltedExecutionInfo.cs
+++ src/Brainf_ckSharp/Models/HaltedExecutionInfo.cs
@@ -8,6 +8,19 @@ namespace Brainf_ckSharp.Models
     public sealed class HaltedExecutionInfo
     {
         /// <summary>
+        /// Creates a new <see cref="HaltedExecutionInfo"/> instance with the specified parameters
+        /// </summary>
+        /// <param name="stackTrace">The stack trace for the current instance</param>
+        /// <param name="haltingOperator">The operator that triggered the halting in the script</param>
+        /// <param name="haltingOffset">The position of the operator that triggered the halting in the executed script</param>
+        internal HaltedExecutionInfo(IReadOnlyList<string> stackTrace, char haltingOperator, int haltingOffset)
+        {
+            StackTrace = stackTrace;
+            HaltingOperator = haltingOperator;
+            HaltingOffset = haltingOffset;
+        }
+
+        /// <summary>
         /// Gets the stack trace for the current instance
         /// </summary>
         public IReadOnlyList<string> StackTrace { get; }
@@ -22,18 +35,5 @@ namespace Brainf_ckSharp.Models
         /// </summary>
         /// <remarks>The offset refers to the processed script, with all the comments removed</remarks>
         public int HaltingOffset { get; }
-
-        /// <summary>
-        /// Creates a new <see cref="HaltedExecutionInfo"/> instance with the specified parameters
-        /// </summary>
-        /// <param name="stackTrace">The stack trace for the current instance</param>
-        /// <param name="haltingOperator">The operator that triggered the halting in the script</param>
-        /// <param name="haltingOffset">The position of the operator that triggered the halting in the executed script</param>
-        internal HaltedExecutionInfo(IReadOnlyList<string> stackTrace, char haltingOperator, int haltingOffset)
-        {
-            StackTrace = stackTrace;
-            HaltingOperator = haltingOperator;
-            HaltingOffset = haltingOffset;
-        }
     }
 }
--- src/Brainf_ckSharp/Models/InterpreterResult.cs
+++ src/Brainf_ckSharp/Models/InterpreterResult.cs
@@ -11,6 +11,40 @@ namespace Brainf_ckSharp.Models
     public sealed class InterpreterResult
     {
         /// <summary>
+        /// Creates a new <see cref="InterpreterResult"/> instance with the specified parameters
+        /// </summary>
+        /// <param name="sourceCode">The original source code for the interpreted script</param>
+        /// <param name="exitCode">The exit code of the interpreter result</param>
+        /// <param name="exceptionInfo">The debug info for the current script, if available</param>
+        /// <param name="machineState">The resulting memory state after running the script</param>
+        /// <param name="functions">The sequence of functions that were defined when running the script</param>
+        /// <param name="stdin">The stdin buffer used to run the script</param>
+        /// <param name="stdout">The resulting stdout buffer after running the script</param>
+        /// <param name="elapsedTime">The execution time for the interpreted script</param>
+        /// <param name="totalOperations">The total numer of evaluated operators for the current result</param>
+        internal InterpreterResult(
+            string sourceCode,
+            ExitCode exitCode,
+            HaltedExecutionInfo? exceptionInfo,
+            IReadOnlyMachineState machineState,
+            IReadOnlyList<FunctionDefinition> functions,
+            string stdin,
+            string stdout,
+            TimeSpan elapsedTime,
+            int totalOperations)
+        {
+            SourceCode = sourceCode;
+            ExitCode = exitCode;
+            ExceptionInfo = exceptionInfo;
+            MachineState = machineState;
+            Functions = functions;
+            Stdin = stdin;
+            Stdout = stdout;
+            ElapsedTime = elapsedTime;
+            TotalOperations = totalOperations;
+        }
+
+        /// <summary>
         /// Gets the original source code for the interpreted script
         /// </summary>
         public string SourceCode { get; }
@@ -54,39 +88,5 @@ namespace Brainf_ckSharp.Models
         /// Gets the total numer of evaluated operators for the current result
         /// </summary>
         public int TotalOperations { get; }
-
-        /// <summary>
-        /// Creates a new <see cref="InterpreterResult"/> instance with the specified parameters
-        /// </summary>
-        /// <param name="sourceCode">The original source code for the interpreted script</param>
-        /// <param name="exitCode">The exit code of the interpreter result</param>
-        /// <param name="exceptionInfo">The debug info for the current script, if available</param>
-        /// <param name="machineState">The resulting memory state after running the script</param>
-        /// <param name="functions">The sequence of functions that were defined when running the script</param>
-        /// <param name="stdin">The stdin buffer used to run the script</param>
-        /// <param name="stdout">The resulting stdout buffer after running the script</param>
-        /// <param name="elapsedTime">The execution time for the interpreted script</param>
-        /// <param name="totalOperations">The total numer of evaluated operators for the current result</param>
-        internal InterpreterResult(
-            string sourceCode,
-            ExitCode exitCode,
-            HaltedExecutionInfo? exceptionInfo,
-            IReadOnlyMachineState machineState,
-            IReadOnlyList<FunctionDefinition> functions,
-            string stdin,
-            string stdout,
-            TimeSpan elapsedTime,
-            int totalOperations)
-        {
-            SourceCode = sourceCode;
-            ExitCode = exitCode;
-            ExceptionInfo = exceptionInfo;
-            MachineState = machineState;
-            Functions = functions;
-            Stdin = stdin;
-            Stdout = stdout;
-            ElapsedTime = elapsedTime;
-            TotalOperations = totalOperations;
-        }
     }
 }
