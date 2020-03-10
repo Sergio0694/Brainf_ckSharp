991991e036126ef35f261140a2b7aa85ccdd0436
Sergio Pedri <sergio0694@live.com>
2020-03-10 23:55:14 +0100

    Added base UWP profiler project

---
+++ profiling/Brainf_ckSharp.Uwp.Profiler/MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace Brainf_ckSharp.Uwp.Profiler
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
