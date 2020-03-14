647f113c31835817c04e8bd5d849237e574f95a0
Sergio Pedri <sergio0694@live.com>
2020-03-14 18:39:03 +0100

    Added initial draft for IdeSessionViewModel

---
+++ src/Brainf_ckSharp.Uwp/Enums/InterpreterSessionSection.cs
@@ -0,0 +1,54 @@
+﻿namespace Brainf_ckSharp.Uwp.Enums
+{
+    /// <summary>
+    /// An <see cref="object"/> based <see langword="enum"/> that indicates the kind of info to show about an interpreter session
+    /// </summary>
+    /// <remarks>This type is not a value type to avoid repeated boxing</remarks>
+    public sealed class InterpreterSessionSection
+    {
+        /// <summary>
+        /// The type of exception raised
+        /// </summary>
+        public static readonly InterpreterSessionSection ExceptionType = new InterpreterSessionSection();
+
+        /// <summary>
+        /// The error location
+        /// </summary>
+        public static readonly InterpreterSessionSection ErrorLocation = new InterpreterSessionSection();
+
+        /// <summary>
+        /// The currently hit breakpoint
+        /// </summary>
+        public static readonly InterpreterSessionSection BreakpointReached = new InterpreterSessionSection();
+
+        /// <summary>
+        /// The stack trace currently available
+        /// </summary>
+        public static readonly InterpreterSessionSection StackTrace = new InterpreterSessionSection();
+
+        /// <summary>
+        /// The stdout buffer
+        /// </summary>
+        public static readonly InterpreterSessionSection Stdout = new InterpreterSessionSection();
+
+        /// <summary>
+        /// The executed source code
+        /// </summary>
+        public static readonly InterpreterSessionSection SourceCode = new InterpreterSessionSection();
+
+        /// <summary>
+        /// The defined functions in the current script
+        /// </summary>
+        public static readonly InterpreterSessionSection FunctionDefinitions = new InterpreterSessionSection();
+
+        /// <summary>
+        /// The current memory state
+        /// </summary>
+        public static readonly InterpreterSessionSection MemoryState = new InterpreterSessionSection();
+
+        /// <summary>
+        /// The statistics for the current session
+        /// </summary>
+        public static readonly InterpreterSessionSection Statistics = new InterpreterSessionSection();
+    }
+}
---
+++ src/Brainf_ckSharp.Uwp/ViewModels/Controls/SubPages/IdeSessionViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Brainf_ckSharp.Enums;
+using Brainf_ckSharp.Models;
+using Brainf_ckSharp.Uwp.Enums;
+using Brainf_ckSharp.Uwp.ViewModels.Abstract.Collections;
+
+#nullable enable
+
+namespace Brainf_ckSharp.Uwp.ViewModels.Controls.SubPages
+{
+    public sealed class IdeSessionViewModel : GroupedItemsCollectionViewModelBase<InterpreterSessionSection, InterpreterResult>
+    {
+        /// <summary>
+        /// Loads the currently available code samples and recently used files
+        /// </summary>
+        public async Task LoadDataAsync(Task<InterpreterResult> task)
+        {
+            InterpreterResult result = await task;
+
+            Source.Clear();
+
+            /* The order of items in the result view is as follows:
+             * - (optional) Exception type
+             * - (optional) Stdout buffer
+             * - (optional) Error location
+             * - (optional) Breakpoint location
+             * - (optional) Stack trace
+             * - Source code
+             * - (optional) Function definitions
+             * - Memory state
+             * - Statistics
+             *
+             * Each group stores the type of section it represents, so that
+             * a template selector can be used in the view. The value of each
+             * group is the the whole session result, as it contains all the
+             * available info for the current script execution.
+             * Each template is responsible for extracting info from it
+             * and display according to its own function and section type. */
+            if (!result.ExitCode.HasFlag(ExitCode.Success)) Source.Add(InterpreterSessionSection.ExceptionType, result);
+            if (result.Stdout.Length > 0) Source.Add(InterpreterSessionSection.Stdout, result);
+            
+            if (result.ExitCode.HasFlag(ExitCode.ExceptionThrown)) Source.Add(InterpreterSessionSection.ErrorLocation, result);
+            else if (result.ExitCode.HasFlag(ExitCode.BreakpointReached)) Source.Add(InterpreterSessionSection.BreakpointReached, result);
+
+            if (result.ExitCode.HasFlag(ExitCode.ExceptionThrown) ||
+                result.ExitCode.HasFlag(ExitCode.ThresholdExceeded) ||
+                result.ExitCode.HasFlag(ExitCode.BreakpointReached))
+            {
+                Source.Add(InterpreterSessionSection.StackTrace, result);
+            }
+
+            Source.Add(InterpreterSessionSection.SourceCode, result);
+
+            if (result.Functions.Count > 0) Source.Add(InterpreterSessionSection.FunctionDefinitions, result);
+
+            Source.Add(InterpreterSessionSection.MemoryState, result);
+
+            Source.Add(InterpreterSessionSection.Statistics, result);
+        }
+    }
+}
