1fc5fbc0bfd63b35152687403a6770713fa1e3f6
Sergio Pedri <sergio0694@live.com>
2020-04-21 20:09:23 +0200

    Added new services projects

---
+++ src/Brainf_ckSharp.Services.Uwp/ClipboardService.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.DataTransfer;
+using Brainf_ckSharp.Services;
+
+#nullable enable
+
+namespace Brainf_ckSharp.Uwp.Services.Clipboard
+{
+    /// <summary>
+    /// A <see langword="class"/> that interacts with the system clipboard
+    /// </summary>
+    public sealed class ClipboardService : IClipboardService
+    {
+        /// <inheritdoc/>
+        public bool TryCopy(string text, bool flush = true)
+        {
+            try
+            {
+                DataPackage package = new DataPackage { RequestedOperation = DataPackageOperation.Copy };
+                package.SetText(text);
+                Windows.ApplicationModel.DataTransfer.Clipboard.SetContent(package);
+
+                if (flush) Windows.ApplicationModel.DataTransfer.Clipboard.Flush();
+
+                return true;
+            }
+            catch
+            {
+                // Whops!
+                return false;
+            }
+        }
+
+        /// <inheritdoc/>
+        public async Task<string?> TryGetTextAsync()
+        {
+            try
+            {
+                DataPackageView view = Windows.ApplicationModel.DataTransfer.Clipboard.GetContent();
+
+                // Try to extract the requested content
+                string? item;
+                if (view.Contains(StandardDataFormats.Text))
+                {
+                    item = await view.GetTextAsync();
+                    view.ReportOperationCompleted(DataPackageOperation.Copy);
+                }
+                else item = null;
+
+                return item;
+            }
+            catch
+            {
+                // Y u do dis?
+                return null;
+            }
+        }
+    }
+}
---
+++ src/Brainf_ckSharp.Services/IShareService.cs
@@ -0,0 +1,15 @@
+﻿namespace Brainf_ckSharp.Services
+{
+    /// <summary>
+    /// The default <see langword="interface"/> for the a service that can share data from the current application
+    /// </summary>
+    public interface IShareService
+    {
+        /// <summary>
+        /// Shares a file from the current applicaation
+        /// </summary>
+        /// <param name="title">The title of the file to share</param>
+        /// <param name="file">The file to share</param>
+        void Share(string title, IFile file);
+    }
+}
