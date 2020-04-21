cc05cbee592d72347ac95f5a2850293a67ab1c55
Sergio Pedri <sergio0694@live.com>
2020-04-21 13:38:44 +0200

    Added IFile and File abstractions

---
+++ src/Brainf_ckSharp.Uwp/Services/Files/File.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Brainf_ckSharp.Uwp.Services.Files
+{
+    /// <summary>
+    /// A <see langword="class"/> implementing a UWP version of <see cref="IFile"/>.
+    /// </summary>
+    public sealed class File : IFile
+    {
+        /// <summary>
+        /// The underlying <see cref="StorageFile"/> instance in use.
+        /// </summary>
+        private readonly StorageFile file;
+
+        /// <summary>
+        /// Creates a new <see cref="StorageFile"/> instance.
+        /// </summary>
+        /// <param name="file">The input <see cref="StorageFile"/> instance to wrap.</param>
+        public File(StorageFile file)
+        {
+            this.file = file;
+        }
+
+        /// <inheritdoc/>
+        public string Path => file.Path;
+
+        /// <inheritdoc/>
+        public Task<Stream> OpenStreamForReadAsync()
+        {
+            return file.OpenStreamForReadAsync();
+        }
+
+        /// <inheritdoc/>
+        public Task<Stream> OpenStreamForWriteAsync()
+        {
+            return file.OpenStreamForWriteAsync();
+        }
+    }
+}
---
+++ src/Brainf_ckSharp.Uwp/Services/Files/IFile.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace Brainf_ckSharp.Uwp.Services.Files
+{
+    /// <summary>
+    /// An <see langword="interface"/> representing a file.
+    /// </summary>
+    public interface IFile
+    {
+        /// <summary>
+        /// Gets the path of the current file
+        /// </summary>
+        string Path { get; }
+
+        /// <summary>
+        /// Opens a <see cref="Stream"/> for reading from the file
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> returning the requested <see cref="Stream"/></returns>
+        Task<Stream> OpenStreamForReadAsync();
+
+        /// <summary>
+        /// Opens a <see cref="Stream"/> for writing to the file
+        /// </summary>
+        /// <returns>A <see cref="Task{TResult}"/> returning the requested <see cref="Stream"/></returns>
+        Task<Stream> OpenStreamForWriteAsync();
+    }
+}
