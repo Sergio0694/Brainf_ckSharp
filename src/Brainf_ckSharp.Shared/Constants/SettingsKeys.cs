6edb5fa6341c9df7adbf9dce8c4d5753103592f5
Sergio Pedri <sergio0694@live.com>
2020-04-22 17:37:33 +0200

    Added settings view model

--- src/Brainf_ckSharp.Shared/Constants/SettingsKeys.cs
+++ src/Brainf_ckSharp.Shared/Constants/SettingsKeys.cs
@@ -6,13 +6,68 @@
     public static class SettingsKeys
     {
         /// <summary>
+        /// Indicates whether or not to autosave documents, uses a <see cref="bool"/>
+        /// </summary>
+        public const string AutosaveDocuments = nameof(AutosaveDocuments);
+
+        /// <summary>
+        /// Indicates whether to protect from closing the app with unsaved changes, uses a <see cref="bool"/>
+        /// </summary>
+        public const string ProtectUnsavedChanges = nameof(ProtectUnsavedChanges);
+
+        /// <summary>
+        /// Indicates whether or not to automatically indent brackets in the IDE, uses a <see cref="bool"/>
+        /// </summary>
+        public const string AutoindentBrackets = nameof(AutoindentBrackets);
+
+        /// <summary>
+        /// Indicates whether to place open brackets on a new line, uses a <see cref="bool"/>
+        /// </summary>
+        public const string BracketsOnNewLine = nameof(BracketsOnNewLine);
+
+        /// <summary>
+        /// Indicates the tab length, uses an <see cref="int"/>
+        /// </summary>
+        public const string TabLength = nameof(TabLength);
+
+        /// <summary>
         /// Indicates the index of the current syntax highlight theme, uses an <see cref="int"/>
         /// </summary>
         public const string Theme = nameof(Theme);
 
         /// <summary>
+        /// Indicates whether to render whitespace characters, uses a <see cref="bool"/>
+        /// </summary>
+        public const string RenderWhitespaces = nameof(RenderWhitespaces);
+
+        /// <summary>
+        /// Indicates whether to enable the Windows timeline, uses a <see cref="bool"/>
+        /// </summary>
+        public const string EnableTimeline = nameof(EnableTimeline);
+
+        /// <summary>
+        /// Indicates the index of the starting page, uses an <see cref="int"/>
+        /// </summary>
+        public const string StartingPage = nameof(StartingPage);
+
+        /// <summary>
         /// Indicates whether or not to automatically clear the stdin buffer whenever it's requested, uses a <see cref="bool"/>
         /// </summary>
         public const string ClearStdinBufferOnRequest = nameof(ClearStdinBufferOnRequest);
+
+        /// <summary>
+        /// Indicates whether or not to show the PBrain buttons, uses a <see cref="bool"/>
+        /// </summary>
+        public const string ShowPBrainButtons = nameof(ShowPBrainButtons);
+
+        /// <summary>
+        /// Indicates the overflow mode currently in use, uses a <see cref="Brainf_ckSharp.Enums.OverflowMode"/>
+        /// </summary>
+        public const string OverflowMode = nameof(OverflowMode);
+
+        /// <summary>
+        /// Indicates the memory size for the interpreter, uses an <see cref="int"/>
+        /// </summary>
+        public const string MemorySize = nameof(MemorySize);
     }
 }
---
+++ src/Brainf_ckSharp.Shared/ViewModels/Controls/SubPages/SettingsViewModel.cs
@@ -0,0 +1,198 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
+using Brainf_ckSharp.Enums;
+using Brainf_ckSharp.Services;
+using Brainf_ckSharp.Shared.Constants;
+using Brainf_ckSharp.Shared.Enums;
+using Brainf_ckSharp.Shared.Extensions.System.Collections.ObjectModel;
+using Brainf_ckSharp.Shared.ViewModels.Abstract;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
+
+namespace Brainf_ckSharp.Shared.ViewModels.Controls.SubPages
+{
+    public sealed class SettingsViewModel : ItemsCollectionViewModelBase<ObservableGroup<SettingsSection, SettingsViewModel>>
+    {
+        /// <summary>
+        /// Creates a new <see cref="SettingsViewModel"/> instance
+        /// </summary>
+        public SettingsViewModel()
+        {
+            Source.Add(SettingsSection.Ide, this);
+            Source.Add(SettingsSection.UI, this);
+            Source.Add(SettingsSection.Interpreter, this);
+        }
+
+        /// <summary>
+        /// Gets a setting with the specified name
+        /// </summary>
+        /// <typeparam name="T">The type of setting value to retrieve</typeparam>
+        /// <param name="name">The setting key to retrieve</param>
+        /// <returns>The value of the setting with the specified name</returns>
+        [Pure]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private static T Get<T>(string name)
+        {
+            return Ioc.Default.GetRequiredService<ISettingsService>().GetValue<T>(name);
+        }
+
+        /// <summary>
+        /// A proxy for <see cref="Microsoft.Toolkit.Mvvm.ComponentModel.ObservableObject.Set{T}"/> that
+        /// also overwrites the value stored in the local settings when a property changes
+        /// </summary>
+        /// <typeparam name="T">The type of setting to set</typeparam>
+        /// <param name="oldValue">The previous setting value</param>
+        /// <param name="value">The new value to set</param>
+        /// <param name="name">The name of the setting that changed</param>
+        private new void Set<T>(ref T oldValue, T value, [CallerMemberName] string name = null!)
+        {
+            if (base.Set(ref oldValue, value, name))
+            {
+                Ioc.Default.GetRequiredService<ISettingsService>().SetValue(name, value);
+            }
+        }
+
+        private bool _AutosaveDocuments = Get<bool>(nameof(AutosaveDocuments));
+
+        /// <summary>
+        /// Exposes the <see cref="SettingsKeys.AutosaveDocuments"/> setting
+        /// </summary>
+        public bool AutosaveDocuments
+        {
+            get => _AutosaveDocuments;
+            set => Set(ref _AutosaveDocuments, value);
+        }
+
+        private bool _ProtectUnsavedChanges = Get<bool>(nameof(ProtectUnsavedChanges));
+
+        /// <summary>
+        /// Exposes the <see cref="SettingsKeys.ProtectUnsavedChanges"/> setting
+        /// </summary>
+        public bool ProtectUnsavedChanges
+        {
+            get => _ProtectUnsavedChanges;
+            set => Set(ref _ProtectUnsavedChanges, value);
+        }
+
+        private bool _AutoindentBrackets = Get<bool>(nameof(AutoindentBrackets));
+
+        /// <summary>
+        /// Exposes the <see cref="SettingsKeys.AutoindentBrackets"/> setting
+        /// </summary>
+        public bool AutoindentBrackets
+        {
+            get => _AutoindentBrackets;
+            set => Set(ref _AutoindentBrackets, value);
+        }
+
+        private bool _BracketsOnNewLine = Get<bool>(nameof(BracketsOnNewLine));
+
+        /// <summary>
+        /// Exposes the <see cref="SettingsKeys.BracketsOnNewLine"/> setting
+        /// </summary>
+        public bool BracketsOnNewLine
+        {
+            get => _BracketsOnNewLine;
+            set => Set(ref _BracketsOnNewLine, value);
+        }
+
+        /// <summary>
+        /// Gets the collection of the available tab lengths
+        /// </summary>
+        public IReadOnlyCollection<int> TabLengthOptions { get; } = new[] { 4, 6, 8, 10, 12 };
+
+        private int _TabLength = Get<int>(nameof(TabLength));
+
+        /// <summary>
+        /// Exposes the <see cref="SettingsKeys.TabLength"/> setting
+        /// </summary>
+        public int TabLength
+        {
+            get => _TabLength;
+            set => Set(ref _TabLength, value);
+        }
+
+        private bool _RenderWhitespaces = Get<bool>(nameof(RenderWhitespaces));
+
+        /// <summary>
+        /// Exposes the <see cref="SettingsKeys.RenderWhitespaces"/> setting
+        /// </summary>
+        public bool RenderWhitespaces
+        {
+            get => _RenderWhitespaces;
+            set => Set(ref _RenderWhitespaces, value);
+        }
+
+        private bool _EnableTimeline = Get<bool>(nameof(EnableTimeline));
+
+        /// <summary>
+        /// Exposes the <see cref="SettingsKeys.EnableTimeline"/> setting
+        /// </summary>
+        public bool EnableTimeline
+        {
+            get => _EnableTimeline;
+            set => Set(ref _EnableTimeline, value);
+        }
+
+        private int _StartingPage = Get<int>(nameof(StartingPage));
+
+        /// <summary>
+        /// Exposes the <see cref="SettingsKeys.StartingPage"/> setting
+        /// </summary>
+        public int StartingPage
+        {
+            get => _StartingPage;
+            set => Set(ref _StartingPage, value);
+        }
+
+        private bool _ClearStdinBufferOnRequest = Get<bool>(nameof(ClearStdinBufferOnRequest));
+
+        /// <summary>
+        /// Exposes the <see cref="SettingsKeys.ClearStdinBufferOnRequest"/> setting
+        /// </summary>
+        public bool ClearStdinBufferOnRequest
+        {
+            get => _ClearStdinBufferOnRequest;
+            set => Set(ref _ClearStdinBufferOnRequest, value);
+        }
+
+        private bool _ShowPBrainButtons = Get<bool>(nameof(ShowPBrainButtons));
+
+        /// <summary>
+        /// Exposes the <see cref="SettingsKeys.ShowPBrainButtons"/> setting
+        /// </summary>
+        public bool ShowPBrainButtons
+        {
+            get => _ShowPBrainButtons;
+            set => Set(ref _ShowPBrainButtons, value);
+        }
+
+        private OverflowMode _OverflowMode = Get<OverflowMode>(nameof(OverflowMode));
+
+        /// <summary>
+        /// Exposes the <see cref="SettingsKeys.OverflowMode"/> setting
+        /// </summary>
+        public OverflowMode OverflowMode
+        {
+            get => _OverflowMode;
+            set => Set(ref _OverflowMode, value);
+        }
+
+        /// <summary>
+        /// Gets the collection of the available tab lengths
+        /// </summary>
+        public IReadOnlyCollection<int> MemorySizeOptions { get; } = new[] { 32, 64, 128, 256 };
+
+        private int _MemorySize = Get<int>(nameof(MemorySize));
+
+        /// <summary>
+        /// Exposes the <see cref="SettingsKeys.MemorySize"/> setting
+        /// </summary>
+        public int MemorySize
+        {
+            get => _MemorySize;
+            set => Set(ref _MemorySize, value);
+        }
+    }
+}
