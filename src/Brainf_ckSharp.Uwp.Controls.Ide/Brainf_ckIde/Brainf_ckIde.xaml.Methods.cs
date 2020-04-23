ce7ae0462da409a69e5c2f2ea6bd18c5c95625c0
Sergio Pedri <sergio0694@live.com>
2020-04-23 21:59:41 +0200

    Added Brainf_ckIde.GetBreakpoints() method

--- src/Brainf_ckSharp.Uwp.Controls.Ide/Brainf_ckIde/Brainf_ckIde.xaml.Methods.cs
+++ src/Brainf_ckSharp.Uwp.Controls.Ide/Brainf_ckIde/Brainf_ckIde.xaml.Methods.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Buffers;
 using System.Diagnostics.Contracts;
+using System.Runtime.CompilerServices;
 using Windows.UI.Text;
+using Microsoft.Toolkit.HighPerformance.Buffers;
 
 namespace Brainf_ckSharp.Uwp.Controls.Ide
 {
@@ -61,6 +64,32 @@ namespace Brainf_ckSharp.Uwp.Controls.Ide
         public void DeleteCharacter()
         {
             CodeEditBox.DeleteSelectionOrCharacter();
+        }
+
+        /// <summary>
+        /// Gets a buffer with the existing breakpoints (their line numbers)
+        /// </summary>
+        /// <returns>A <see cref="MemoryOwner{T}"/> instance with the line numbers with a breakpoint.</returns>
+        [Pure]
+        public IMemoryOwner<int> GetBreakpoints()
+        {
+            int count = BreakpointIndicators.Count;
+
+            if (count == 0) return MemoryOwner<int>.Empty;
+
+            // Rent a buffer to copy the line numbers with a breakpoint
+            MemoryOwner<int> buffer = MemoryOwner<int>.Allocate(count);
+
+            ref int r0 = ref buffer.DangerousGetReference();
+            int i = 0;
+
+            // Copy the existing breakpoints
+            foreach (var pair in BreakpointIndicators)
+            {
+                Unsafe.Add(ref r0, i++) = pair.Key;
+            }
+
+            return buffer;
         }
     }
 }
