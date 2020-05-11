e4ae8a3214dccc415dcf1fd35ed0544e10c89a9e
Sergio Pedri <sergio0694@live.com>
2020-05-11 15:32:13 +0200

    Enabled RELEASE run in the IDE view

--- src/Brainf_ckSharp.Shared/ViewModels/Controls/SubPages/IdeResultSubPageViewModel.cs
+++ src/Brainf_ckSharp.Shared/ViewModels/Controls/SubPages/IdeResultSubPageViewModel.cs
@@ -37,6 +37,11 @@ namespace Brainf_ckSharp.Shared.ViewModels.Controls.SubPages
         private CancellationTokenSource? _CancellationTokenSource;
 
         /// <summary>
+        /// The <see cref="InterpreterSession"/> to use, when in DEBUG mode
+        /// </summary>
+        private InterpreterSession? _DebugSession;
+
+        /// <summary>
         /// Creates a new <see cref="IdeResultSubPageViewModel"/> instance
         /// </summary>
         public IdeResultSubPageViewModel()
@@ -61,9 +66,14 @@ namespace Brainf_ckSharp.Shared.ViewModels.Controls.SubPages
         public ICommand LoadDataCommand { get; }
 
         /// <inheritdoc/>
-        protected override void OnDeactivated()
+        protected override async void OnDeactivated()
         {
             _CancellationTokenSource?.Cancel();
+
+            using (await LoadingMutex.LockAsync())
+            {
+                _DebugSession?.Dispose();
+            }
         }
 
         /// <summary>
@@ -83,12 +93,11 @@ namespace Brainf_ckSharp.Shared.ViewModels.Controls.SubPages
                 int memorySize = Ioc.Default.GetRequiredService<ISettingsService>().GetValue<int>(SettingsKeys.MemorySize);
                 OverflowMode overflowMode = Ioc.Default.GetRequiredService<ISettingsService>().GetValue<OverflowMode>(SettingsKeys.OverflowMode);
 
-                // Run the code on a background thread
-                InterpreterResult result = await Task.Run(() =>
+                // Run in RELEASE mode
+                if (Breakpoints is null)
                 {
-                    if (Breakpoints is null)
+                    InterpreterResult result = await Task.Run(() =>
                     {
-                        // Run in RELEASE mode
                         return Brainf_ckInterpreter
                             .CreateReleaseConfiguration()
                             .WithSource(Script!)
@@ -98,72 +107,87 @@ namespace Brainf_ckSharp.Shared.ViewModels.Controls.SubPages
                             .WithExecutionToken(_CancellationTokenSource.Token)
                             .TryRun()
                             .Value!;
-                    }
+                    });
 
+                    LoadResults(result);
+                }
+                else
+                {
                     // Run in DEBUG mode
-                    using InterpreterSession session = Brainf_ckInterpreter
-                        .CreateDebugConfiguration()
-                        .WithSource(Script!)
-                        .WithStdin(stdin)
-                        .WithBreakpoints(Breakpoints.Memory)
-                        .WithMemorySize(memorySize)
-                        .WithOverflowMode(overflowMode)
-                        .WithExecutionToken(_CancellationTokenSource.Token)
-                        .TryRun()
-                        .Value!;
+                    _DebugSession = await Task.Run(() =>
+                    {
+                        
+                        return Brainf_ckInterpreter
+                            .CreateDebugConfiguration()
+                            .WithSource(Script!)
+                            .WithStdin(stdin)
+                            .WithBreakpoints(Breakpoints.Memory)
+                            .WithMemorySize(memorySize)
+                            .WithOverflowMode(overflowMode)
+                            .WithExecutionToken(_CancellationTokenSource.Token)
+                            .TryRun()
+                            .Value!;
+                    });
 
-                    session.MoveNext();
+                    _DebugSession.MoveNext();
 
-                    return session.Current;
-                });
-
-                Source.Clear();
-
-                // A function used to quickly add a specific section to the current collection
-                void AddToSource(IdeResultSection section)
-                {
-                    var model = new IdeResultWithSectionInfo(section, result);
-
-                    Source.Add(section, model);
+                    LoadResults(_DebugSession.Current);
                 }
-
-                /* The order of items in the result view is as follows:
-                 * - (optional) Exception type
-                 * - (optional) Stdout buffer
-                 * - (optional) Error location
-                 * - (optional) Breakpoint location
-                 * - (optional) Stack trace
-                 * - Source code
-                 * - (optional) Function definitions
-                 * - Memory state
-                 * - Statistics
-                 *
-                 * Each group stores the type of section it represents, so that
-                 * a template selector can be used in the view. The value of each
-                 * group is the the whole session result, as it contains all the
-                 * available info for the current script execution.
-                 * Each template is responsible for extracting info from it
-                 * and display according to its own function and section type. */
-                if (!result.ExitCode.HasFlag(ExitCode.Success)) AddToSource(IdeResultSection.ExceptionType);
-                if (result.Stdout.Length > 0) AddToSource(IdeResultSection.Stdout);
-
-                if (result.ExitCode.HasFlag(ExitCode.ExceptionThrown)) AddToSource(IdeResultSection.ErrorLocation);
-                else if (result.ExitCode.HasFlag(ExitCode.BreakpointReached)) AddToSource(IdeResultSection.BreakpointReached);
-
-                if (result.ExitCode.HasFlag(ExitCode.ExceptionThrown) ||
-                    result.ExitCode.HasFlag(ExitCode.ThresholdExceeded) ||
-                    result.ExitCode.HasFlag(ExitCode.BreakpointReached))
-                {
-                    AddToSource(IdeResultSection.StackTrace);
-                }
-
-                AddToSource(IdeResultSection.SourceCode);
-
-                if (result.Functions.Count > 0) AddToSource(IdeResultSection.FunctionDefinitions);
-
-                AddToSource(IdeResultSection.MemoryState);
-                AddToSource(IdeResultSection.Statistics);
             }
+        }
+
+        /// <summary>
+        /// Displays the result for an execution run
+        /// </summary>
+        /// <param name="result">The <see cref="InterpreterResult"/> instance to display the results for</param>
+        private void LoadResults(InterpreterResult result)
+        {
+            Source.Clear();
+
+            // A function used to quickly add a specific section to the current collection
+            void AddToSource(IdeResultSection section)
+            {
+                var model = new IdeResultWithSectionInfo(section, result);
+
+                Source.Add(section, model);
+            }
+
+            /* The order of items in the result view is as follows:
+             * - (optional) Exception type
+             * - (optional) Stdout buffer
+             * - (optional) Error location
+             * - (optional) Breakpoint location
+             * - (optional) Stack trace
+             * - Source code
+             * - (optional) Function definitions
+             * - Memory state
+             * - Statistics
+             *
+             * Each group stores the type of section it represents, so that
+             * a template selector can be used in the view. The value of each
+             * group is the the whole session result, as it contains all the
+             * available info for the current script execution.
+             * Each template is responsible for extracting info from it
+             * and display according to its own function and section type. */
+            if (!result.ExitCode.HasFlag(ExitCode.Success)) AddToSource(IdeResultSection.ExceptionType);
+            if (result.Stdout.Length > 0) AddToSource(IdeResultSection.Stdout);
+
+            if (result.ExitCode.HasFlag(ExitCode.ExceptionThrown)) AddToSource(IdeResultSection.ErrorLocation);
+            else if (result.ExitCode.HasFlag(ExitCode.BreakpointReached)) AddToSource(IdeResultSection.BreakpointReached);
+
+            if (result.ExitCode.HasFlag(ExitCode.ExceptionThrown) ||
+                result.ExitCode.HasFlag(ExitCode.ThresholdExceeded) ||
+                result.ExitCode.HasFlag(ExitCode.BreakpointReached))
+            {
+                AddToSource(IdeResultSection.StackTrace);
+            }
+
+            AddToSource(IdeResultSection.SourceCode);
+
+            if (result.Functions.Count > 0) AddToSource(IdeResultSection.FunctionDefinitions);
+
+            AddToSource(IdeResultSection.MemoryState);
+            AddToSource(IdeResultSection.Statistics);
         }
     }
 }
