1318c48d55506d47d31f27b666a0c8cf1a42bca4
Sergio Pedri <sergio0694@live.com>
2020-05-15 17:44:27 +0200

    Added live theme switching to console commands

--- src/Brainf_ckSharp.Uwp/Views/ConsoleView.xaml.cs
+++ src/Brainf_ckSharp.Uwp/Views/ConsoleView.xaml.cs
@@ -1,4 +1,9 @@
-﻿using Windows.UI.Xaml.Controls;
+﻿using System.Linq;
+using Windows.UI.Xaml.Controls;
+using Brainf_ckSharp.Shared.Enums.Settings;
+using Brainf_ckSharp.Shared.Models.Console;
+using Microsoft.Toolkit.Mvvm.Messaging;
+using Microsoft.Toolkit.Mvvm.Messaging.Messages;
 
 #nullable enable
 
@@ -12,6 +17,23 @@ namespace Brainf_ckSharp.Uwp.Views
         public ConsoleView()
         {
             this.InitializeComponent();
+
+            Messenger.Default.Register<ValueChangedMessage<IdeTheme>>(this, m =>
+            {
+                // We want to render all the displayed commands again using the new theme.
+                // The easy way to do that is to simply retrieve all the existing commands
+                // and simulate an update of their embedded script. This will cause the
+                // attached property to redraw the text with the new syntax highlight
+                // theme, even if the source code is actually the same as it was before.
+                foreach (ConsoleCommand item in ViewModel.Source.OfType<ConsoleCommand>())
+                {
+                    string text = item.Command;
+
+                    item.Command = string.Empty;
+
+                    item.Command = text;
+                }
+            });
         }
     }
 }
