a3265909b9631cb3f626e9dd989253a2afec0e1c
Sergio Pedri <sergio0694@live.com>
2020-05-31 19:59:57 +0200

    Added analytics services (AppCenter)

---
+++ src/Brainf_ckSharp.Services/IAnalyticsService.cs
@@ -0,0 +1,21 @@
+﻿namespace Brainf_ckSharp.Services
+{
+    /// <summary>
+    /// The default <see langword="interface"/> for an analytics service
+    /// </summary>
+    public interface IAnalyticsService
+    {
+        /// <summary>
+        /// Initializes the current <see cref="IAnalyticsService"/> instance
+        /// </summary>
+        /// <param name="secret">The secret token to use to initialize the service</param>
+        void Initialize(string secret);
+
+        /// <summary>
+        /// Logs an event with a specified title and optional properties
+        /// </summary>
+        /// <param name="title">The title of the event to track</param>
+        /// <param name="data">The optional event properties</param>
+        void Log(string title, params (string Property, string Value)[]? data);
+    }
+}
