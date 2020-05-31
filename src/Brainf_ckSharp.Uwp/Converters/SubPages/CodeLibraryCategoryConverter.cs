b626b30d319eaa98c8840ef487d635c88ed5f49a
Sergio Pedri <sergio0694@live.com>
2020-05-31 18:41:39 +0200

    Added description to headers in code library

--- src/Brainf_ckSharp.Uwp/Converters/SubPages/CodeLibraryCategoryConverter.cs
+++ src/Brainf_ckSharp.Uwp/Converters/SubPages/CodeLibraryCategoryConverter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics.Contracts;
 using Brainf_ckSharp.Shared.Enums;
+using Microsoft.Toolkit.Collections;
 
 namespace Brainf_ckSharp.Uwp.Converters.SubPages
 {
@@ -15,7 +16,7 @@ namespace Brainf_ckSharp.Uwp.Converters.SubPages
         /// <param name="section">The input <see cref="CodeLibrarySection"/> value</param>
         /// <returns>A <see cref="string"/> representing the input <see cref="CodeLibrarySection"/> value</returns>
         [Pure]
-        public static string Convert(CodeLibrarySection section)
+        public static string ConvertSectionName(CodeLibrarySection section)
         {
             return section switch
             {
@@ -23,6 +24,29 @@ namespace Brainf_ckSharp.Uwp.Converters.SubPages
                 CodeLibrarySection.Recent => "Recent",
                 CodeLibrarySection.Samples => "Samples",
                 _ => throw new ArgumentException($"Invalid input value: {section}", nameof(section))
+            };
+        }
+
+        /// <summary>
+        /// Converts a group into its description for a given section
+        /// </summary>
+        /// <param name="group">The input <see cref="IReadOnlyObservableGroup"/> instance</param>
+        /// <returns>A <see cref="string"/> representing the input <see cref="IReadOnlyObservableGroup"/> instance</returns>
+        [Pure]
+        public static string ConvertSectionDescription(IReadOnlyObservableGroup group)
+        {
+            return (CodeLibrarySection)group.Key switch
+            {
+                CodeLibrarySection.Favorites => group.Count > 0
+                    ? $"{group.Count} favorite script{(group.Count > 1 ? "s" : string.Empty)}"
+                    : "No favorite scripts",
+                CodeLibrarySection.Recent => group.Count > 0
+                    ? $"{group.Count} recent script{(group.Count > 1 ? "s" : string.Empty)}"
+                    : "No recent scripts",
+                CodeLibrarySection.Samples => group.Count > 0
+                    ? $"{group.Count} sample script{(group.Count > 1 ? "s" : string.Empty)}"
+                    : "No sample scripts",
+                _ => throw new ArgumentException($"Invalid group value: {group.Key}", nameof(group))
             };
         }
     }
