1e510dacd0fc26e63c2a1067e083720342eca25e
Sergio Pedri <sergio0694@live.com>
2020-06-03 17:10:39 +0200

    Added UI update for virtual keyboard setting

--- src/Brainf_ckSharp.Uwp/Controls/Host/InputPanel/VirtualKeyboard/OperatorsKeyboard.xaml.cs
+++ src/Brainf_ckSharp.Uwp/Controls/Host/InputPanel/VirtualKeyboard/OperatorsKeyboard.xaml.cs
@@ -1,8 +1,15 @@
 ﻿using System;
 using System.Diagnostics;
+using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
+using Brainf_ckSharp.Services;
+using Brainf_ckSharp.Shared;
 using Brainf_ckSharp.Shared.Messages.InputPanel;
+using Brainf_ckSharp.Shared.ViewModels.Controls.SubPages;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Toolkit.Mvvm.DependencyInjection;
 using Microsoft.Toolkit.Mvvm.Messaging;
+using Microsoft.Toolkit.Mvvm.Messaging.Messages;
 
 namespace Brainf_ckSharp.Uwp.Controls.Host.InputPanel.VirtualKeyboard
 {
@@ -14,6 +21,25 @@ namespace Brainf_ckSharp.Uwp.Controls.Host.InputPanel.VirtualKeyboard
         public OperatorsKeyboard()
         {
             this.InitializeComponent();
+
+            bool showPBrainOperators = Ioc.Default.GetRequiredService<ISettingsService>().GetValue<bool>(SettingsKeys.ShowPBrainButtons);
+
+            VisualStateManager.GoToState(this, showPBrainOperators ? nameof(ExtendedState) : nameof(MinimalState), false);
+
+            Messenger.Default.Register<PropertyChangedMessage<bool>>(this, UpdateKeyboardVisualState);
+        }
+
+        /// <summary>
+        /// Updates the UI when the settings for the virtual keyboard is changed
+        /// </summary>
+        /// <param name="message">The <see cref="PropertyChangedMessage{T}"/> instance to receive</param>
+        private void UpdateKeyboardVisualState(PropertyChangedMessage<bool> message)
+        {
+            if (message.Sender.GetType() == typeof(SettingsSubPageViewModel) &&
+                message.PropertyName == nameof(SettingsKeys.ShowPBrainButtons))
+            {
+                VisualStateManager.GoToState(this, message.NewValue ? nameof(ExtendedState) : nameof(MinimalState), false);
+            }
         }
 
         /// <summary>
