2dd79157bef9f1e8a97906e042438cca854ce94d
Sergio Pedri <sergio0694@live.com>
2020-06-03 13:53:09 +0200

    Added tooltip info to background execution runs

--- src/Brainf_ckSharp.Uwp/Converters/ExecutionResultConverter.cs
+++ src/Brainf_ckSharp.Uwp/Converters/ExecutionResultConverter.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 using Brainf_ckSharp.Enums;
 using Brainf_ckSharp.Models;
 using Brainf_ckSharp.Models.Base;
+using Brainf_ckSharp.Uwp.Converters.Console;
 
 #nullable enable
 
@@ -38,6 +39,34 @@ namespace Brainf_ckSharp.Uwp.Converters
         public static bool IsFailedRun(Option<InterpreterResult>? result)
         {
             return result?.Value?.ExitCode != ExitCode.Success;
+        }
+
+        /// <summary>
+        /// Returns a representation of a given <see cref="Option{T}"/> of <see cref="InterpreterResult"/> run
+        /// </summary>
+        /// <param name="result">The input result to convert</param>
+        /// <returns>A representation of the input run</returns>
+        [Pure]
+        public static string? StdoutOrExitCodeDescription(Option<InterpreterResult>? result)
+        {
+            if (result is null) return null;
+
+            // No result if the script is empty
+            if (result.ValidationResult.IsEmptyScript) return null;
+
+            // Show the syntax error, if present
+            if (result.ValidationResult.IsError)
+            {
+                return SyntaxValidationResultConverter.Convert(result.ValidationResult);
+            }
+
+            // Show the exception message, if any
+            if (result.Value!.ExitCode != ExitCode.Success)
+            {
+                return ExitCodeConverter.Convert(result.Value!.ExitCode);
+            }
+
+            return result.Value!.Stdout.Length > 0 ? result.Value!.Stdout : null;
         }
     }
 }
