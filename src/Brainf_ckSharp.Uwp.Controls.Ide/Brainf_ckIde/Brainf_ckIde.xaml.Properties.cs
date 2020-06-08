843dba5156545cb735df851c1f5216e49619f7ef
Sergio Pedri <sergio0694@live.com>
2020-06-08 20:05:28 +0200

    Added bindings in IDE page for syntax error tooltip

--- src/Brainf_ckSharp.Uwp.Controls.Ide/Brainf_ckIde/Brainf_ckIde.xaml.Properties.cs
+++ src/Brainf_ckSharp.Uwp.Controls.Ide/Brainf_ckIde/Brainf_ckIde.xaml.Properties.cs
@@ -13,6 +13,24 @@ namespace Brainf_ckSharp.Uwp.Controls.Ide
         public string Text => CodeEditBox.Text;
 
         /// <summary>
+        /// Gets or sets the title of the tooltip displayed over a syntax error
+        /// </summary>
+        public string SyntaxErrorToolTipTitle
+        {
+            get => (string)GetValue(SyntaxErrorToolTipTitleProperty);
+            set => SetValue(SyntaxErrorToolTipTitleProperty, value);
+        }
+
+        /// <summary>
+        /// Gets or sets the subtitle of the tooltip displayed over a syntax error
+        /// </summary>
+        public string SyntaxErrorToolTipSubtitle
+        {
+            get => (string)GetValue(SyntaxErrorToolTipSubtitleProperty);
+            set => SetValue(SyntaxErrorToolTipSubtitleProperty, value);
+        }
+
+        /// <summary>
         /// Gets or sets the syntax highlight theme to use
         /// </summary>
         public FrameworkElement ContextMenuSecondaryContent
@@ -29,6 +47,26 @@ namespace Brainf_ckSharp.Uwp.Controls.Ide
             get => CodeEditBox.RenderWhitespaceCharacters;
             set => CodeEditBox.RenderWhitespaceCharacters = value;
         }
+
+        /// <summary>
+        /// Gets the dependency property for <see cref="SyntaxErrorToolTipTitle"/>.
+        /// </summary>
+        public static readonly DependencyProperty SyntaxErrorToolTipTitleProperty =
+            DependencyProperty.Register(
+                nameof(SyntaxErrorToolTipTitle),
+                typeof(string),
+                typeof(Brainf_ckIde),
+                new PropertyMetadata(string.Empty));
+
+        /// <summary>
+        /// Gets the dependency property for <see cref="SyntaxErrorToolTipSubtitle"/>.
+        /// </summary>
+        public static readonly DependencyProperty SyntaxErrorToolTipSubtitleProperty =
+            DependencyProperty.Register(
+                nameof(SyntaxErrorToolTipSubtitle),
+                typeof(string),
+                typeof(Brainf_ckIde),
+                new PropertyMetadata(string.Empty));
 
         /// <summary>
         /// Gets or sets the syntax highlight theme to use
