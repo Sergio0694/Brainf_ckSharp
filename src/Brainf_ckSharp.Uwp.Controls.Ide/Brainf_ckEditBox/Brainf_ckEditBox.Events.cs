cda47d5fb98a07bf31c0eb1e9f765e46f28a54eb
Sergio Pedri <sergio0694@live.com>
2020-06-09 13:44:29 +0200

    Added ability to inject draw calls to Brainf_ckEditBox

--- src/Brainf_ckSharp.Uwp.Controls.Ide/Brainf_ckEditBox/Brainf_ckEditBox.Events.cs
+++ src/Brainf_ckSharp.Uwp.Controls.Ide/Brainf_ckEditBox/Brainf_ckEditBox.Events.cs
@@ -1,4 +1,6 @@
 ﻿using Windows.Foundation;
+using Microsoft.Graphics.Canvas.UI;
+using Microsoft.Graphics.Canvas.UI.Xaml;
 
 #nullable enable
 
@@ -15,5 +17,24 @@ namespace Brainf_ckSharp.Uwp.Controls.Ide
         /// Rasised when the cursor position changes
         /// </summary>
         public event TypedEventHandler<Brainf_ckEditBox, CursorPositionChangedEventArgs>? CursorPositionChanged;
+
+        /// <summary>
+        /// Raised whenever the resources for the overlays are being created.
+        /// </summary>
+        public event TypedEventHandler<CanvasControl, CanvasCreateResourcesEventArgs>? CreateOverlayResources;
+
+        /// <summary>T
+        /// Raised whenever the overlays are being drawn. This event can be used to inject
+        /// external overlays, that will automatically be kept in sync with the text displayed.
+        /// </summary>
+        public event TypedEventHandler<CanvasControl, CanvasDrawEventArgs>? DrawOverlays;
+
+        /// <summary>
+        /// Forces a redraw of the text overlays
+        /// </summary>
+        public void InvalidateOverlays()
+        {
+            _TextOverlaysCanvas!.Invalidate();
+        }
     }
 }
