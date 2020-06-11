f0694201fc7db602287a9c929ddd8727c3f736fa
Sergio Pedri <sergio0694@live.com>
2020-06-11 18:40:07 +0200

    Added email service

---
+++ src/Brainf_ckSharp.Services.Uwp/EmailService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using Windows.ApplicationModel.Email;
+
+#nullable enable
+
+namespace Brainf_ckSharp.Services.Uwp
+{
+    /// <summary>
+    /// A <see langword="class"/> that handles emails
+    /// </summary>
+    public sealed class EmailService : IEmailService
+    {
+        /// <inheritdoc/>
+        public async Task TryComposeEmailAsync(string? address = null, string? subject = null, string? body = null)
+        {
+            EmailMessage email = new EmailMessage();
+
+            if (!(address is null)) email.To.Add(new EmailRecipient(address));
+            if (!(subject is null)) email.Subject = subject;
+            if (!(body is null)) email.Body = body;
+
+            // Try to send the email message
+            try
+            {
+                await EmailManager.ShowComposeNewEmailAsync(email);
+            }
+            catch
+            {
+                // Ignore errors
+            }
+        }
+    }
+}
---
+++ src/Brainf_ckSharp.Services/IEmailService.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+
+namespace Brainf_ckSharp.Services
+{
+    /// <summary>
+    /// The default <see langword="interface"/> for a service that handles emails
+    /// </summary>
+    public interface IEmailService
+    {
+        /// <summary>
+        /// Tries to open the compose window for a new email with the specified parameters
+        /// </summary>
+        /// <param name="address">The (optional) target address to send the email to</param>
+        /// <param name="subject">The (optional) subject of the email</param>
+        /// <param name="body">The (optional) body of the email</param>
+        /// <returns>A <see cref="Task"/> that completes when the email has been completed</returns>
+        Task TryComposeEmailAsync(string? address = null, string? subject = null, string? body = null);
+    }
+}
