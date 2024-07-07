c98604a3654c641b216a0e350b8623c4fef197e2
Sergio Pedri <sergio0694@live.com>
2024-07-06 17:14:59 -0700

    Add 'IMachineStateNumberHandler<TValue>' and implementations

---
+++ src/Brainf_ckSharp/Memory/Interfaces/IMachineStateNumberHandler{TValue}.cs
@@ -0,0 +1,33 @@
+using System.Numerics;
+
+#pragma warning disable CS1573
+
+namespace Brainf_ckSharp.Memory.Interfaces;
+
+/// <summary>
+/// An <see langword="interface"/> that is used by mode-specific execution contexts
+/// </summary>
+/// <typeparam name="TValue">The type of values in each memory cell</typeparam>
+internal interface IMachineStateNumberHandler<TValue>
+    where TValue : unmanaged, IBinaryInteger<TValue>
+{
+    /// <inheritdoc cref="IMachineStateExecutionContext{TValue}.TryIncrement()"/>
+    /// <param name="value">The target value to modify.</param>
+    static abstract bool TryIncrement(ref TValue value);
+
+    /// <inheritdoc cref="IMachineStateExecutionContext{TValue}.TryIncrement(int, ref int)"/>
+    /// <param name="value">The target value to modify.</param>
+    static abstract bool TryIncrement(ref TValue value, int count, ref int totalOperations);
+
+    /// <inheritdoc cref="IMachineStateExecutionContext{TValue}.TryDecrement()"/>
+    /// <param name="value">The target value to modify.</param>
+    static abstract bool TryDecrement(ref TValue value);
+
+    /// <inheritdoc cref="IMachineStateExecutionContext{TValue}.TryDecrement(int, ref int)"/>
+    /// <param name="value">The target value to modify.</param>
+    static abstract bool TryDecrement(ref TValue value, int count, ref int totalOperations);
+
+    /// <inheritdoc cref="IMachineStateExecutionContext{TValue}.TryInput"/>
+    /// <param name="value">The target value to modify.</param>
+    static abstract bool TryInput(ref TValue value, char c);
+}
